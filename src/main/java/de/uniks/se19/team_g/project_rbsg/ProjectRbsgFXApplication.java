acbc49c63a84456d6b78fc1eec05b23e5ae3d5d8
Jan Müller <janmueller3698@gmail.com>
2019-05-13 15:45:30 +0200

    Created JavaFX application
    Created the JavaFX application with Spring IoC context

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/ProjectRbsgFXApplication.java
@@ -0,0 +1,62 @@
+package de.uniks.se19.team_g.project_rbsg;
+
+import de.uniks.se19.team_g.project_rbsg.view.SplashImageBuilder;
+import javafx.application.Application;
+import javafx.application.HostServices;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+public class ProjectRbsgFXApplication extends Application {
+
+    private static final int WIDTH = 1336;
+    private static final int HEIGHT = 768;
+
+    private ConfigurableApplicationContext context;
+
+    public static void main(final String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void init() {
+        ApplicationContextInitializer<GenericApplicationContext> contextInitializer = applicationContext -> {
+            applicationContext.registerBean(Application.class, () -> ProjectRbsgFXApplication.this);
+            applicationContext.registerBean(Parameters.class, this::getParameters);
+            applicationContext.registerBean(HostServices.class, this::getHostServices);
+        };
+
+        this.context = new SpringApplicationBuilder()
+                .sources(ProjectRbsgApplication.class)
+                .initializers(contextInitializer)
+                .run(getParameters().getRaw().toArray(new String[0]));
+    }
+
+
+    @Override
+    public void start(final Stage primaryStage) {
+        Pane pane = new Pane();
+        pane.setBackground(new Background(SplashImageBuilder.getSplashImage()));
+
+        Scene scene = new Scene(pane);
+
+        primaryStage.setWidth(1336);
+        primaryStage.setHeight(768);
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() {
+        this.context.close();
+        Platform.exit();
+    }
+}
