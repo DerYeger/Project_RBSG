1b3c7cbfcb0192bb6c0475d4a73fdf805358c6b9
Jan Müller <uk046154@student.uni-kassel.de>
2019-05-14 11:12:33 +0200

    Added simple login form builder test

---
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/view/LoginFormBuilderTests.java
@@ -0,0 +1,17 @@
+package de.uniks.se19.team_g.project_rbsg.view;
+
+import javafx.scene.Node;
+import org.junit.Assert;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import java.io.IOException;
+
+public class LoginFormBuilderTests extends ApplicationTest {
+
+    @Test
+    public void test() throws IOException {
+        final Node loginForm = new LoginFormBuilder().getLoginForm();
+        Assert.assertNotNull(loginForm);
+    }
+}
