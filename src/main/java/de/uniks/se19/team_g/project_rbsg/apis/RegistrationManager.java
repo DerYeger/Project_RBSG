561827a07d2768c24005e594b24fdb4096f84fa4
Juri <juri.lozowoj@freenet.de>
2019-05-14 11:38:08 +0200

    Server Mocking, Abhängigkeiten in den Konstruktor ziehen

--- src/main/java/de/uniks/se19/team_g/project_rbsg/apis/RegistrationManager.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/apis/RegistrationManager.java
@@ -1,17 +1,28 @@
 package de.uniks.se19.team_g.project_rbsg.apis;
 
 import de.uniks.se19.team_g.project_rbsg.model.User;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
 public class RegistrationManager {
 
-    public boolean onRegistration(User user){
+    private RestTemplate restTemplate;
+
+    public RegistrationManager(@Nullable RestTemplate restTemplate) {
+        this.restTemplate = restTemplate == null ? new RestTemplate() : restTemplate;
+    }
+
+    public boolean onRegistration(@NonNull User user){
         final String uri = "https://rbsg.uniks.de/api/user";
 
-        RestTemplate restTemplate = new RestTemplate();
         Map<String, Object> answer = restTemplate.postForObject(uri, user, Map.class);
-        return true;
+        if(answer.get("status").equals("success")){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/test/java/de/uniks/se19/team_g/project_rbsg/RegistrationManagerTest.java
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/RegistrationManagerTest.java
@@ -4,15 +4,33 @@ import de.uniks.se19.team_g.project_rbsg.apis.RegistrationManager;
 import de.uniks.se19.team_g.project_rbsg.model.User;
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.lang.Nullable;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.concurrent.CompletableFuture;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrationManagerTest {
 
     @Test
     public void onRegistrationTest(){
-        User testUser = new User("Keanu", "geheim");
-        RegistrationManager registrationManager = new RegistrationManager();
+        User testUser = new User("Juri", "geheim");
+        RegistrationManager registrationManager = new RegistrationManager(
+                new RestTemplate() {
+                    @Override
+                    public <T> T postForObject(String url, @Nullable Object request, Class<T> responseType,
+                                               Object... uriVariables) throws RestClientException {
+                        Assert.assertTrue(request instanceof User);
+                        Assert.assertEquals(url, "https://rbsg.uniks.de/api/user");
+
+                        Map<String, Object> testAnswer = new HashMap<>();
+                        testAnswer.put("status", "success");
+                        return (T) testAnswer;
+                    }
+                }
+        );
         boolean registrationAnswer = registrationManager.onRegistration(testUser);
         Assert.assertTrue(registrationAnswer);
 
