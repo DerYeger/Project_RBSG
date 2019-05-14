2a152cdb09a4e4ac58a4f96f86df62b8ef287cfe
Juri <juri.lozowoj@freenet.de>
2019-05-14 20:50:34 +0200

    Auto stash before rebase of "origin/develop"
    Added: Error Message when User is already registered

--- src/main/java/de/uniks/se19/team_g/project_rbsg/apis/RegistrationManager.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/apis/RegistrationManager.java
@@ -5,24 +5,47 @@ import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Supplier;
+
+/**
+ * @author Juri Lozowoj
+ */
 
 public class RegistrationManager {
 
+    final String uri = "https://rbsg.uniks.de/api/user";
+
     private RestTemplate restTemplate;
 
-    public RegistrationManager(@Nullable RestTemplate restTemplate) {
-        this.restTemplate = restTemplate == null ? new RestTemplate() : restTemplate;
+    public RegistrationManager(){
+        this.restTemplate = new RestTemplate();
     }
 
-    public boolean onRegistration(@NonNull User user){
-        final String uri = "https://rbsg.uniks.de/api/user";
+    public RegistrationManager(@Nullable RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
-        Map<String, Object> answer = restTemplate.postForObject(uri, user, Map.class);
-        if(answer.get("status").equals("success")){
-            return true;
-        } else {
-            return false;
+    public HashMap<String, Object> onRegistration(@NonNull User user){
+
+        CompletableFuture<HashMap<String, Object>> completableFuture = CompletableFuture.supplyAsync(new Supplier<HashMap<String, Object>>() {
+            @Override
+            public HashMap<String, Object> get(){
+                HashMap<String, Object> serverAnswer = restTemplate.postForObject(uri, user, HashMap.class);
+                return serverAnswer;
+            }
+        });
+
+        try {
+            return completableFuture.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
+        return null;
     }
 }
--- src/main/java/de/uniks/se19/team_g/project_rbsg/model/User.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/model/User.java
@@ -1,11 +1,13 @@
 package de.uniks.se19.team_g.project_rbsg.model;
 
+import org.springframework.lang.NonNull;
+
 public class User {
 
     private String name;
     private String password;
 
-    public User(String name, String password){
+    public User(@NonNull String name, @NonNull String password){
         this.name = name;
         this.password = password;
     }
