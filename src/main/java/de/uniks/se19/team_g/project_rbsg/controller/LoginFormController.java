e67a884a947dc6623180af405756733de86b0541
Omar Sood <goatfryed@gmail.com>
2019-05-14 21:35:51 +0200

    Revert "Merge remote-tracking branch 'origin/master' into feature/Registration"
    This reverts commit 9d72bba792bf827d8959225fa503dc3050f84864, reversing
    changes made to ba26e4a5b4eb679dff28dfdc3b2cf0b0a36fc037.

--- src/main/java/de/uniks/se19/team_g/project_rbsg/controller/LoginFormController.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/controller/LoginFormController.java
@@ -1,17 +1,13 @@
 package de.uniks.se19.team_g.project_rbsg.controller;
 
-import de.uniks.se19.team_g.project_rbsg.apis.RegistrationManager;
-import de.uniks.se19.team_g.project_rbsg.model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import org.springframework.stereotype.Controller;
 
 import javax.validation.constraints.NotNull;
-import java.util.HashMap;
 
 @Controller
 public class LoginFormController {
@@ -42,23 +38,7 @@ public class LoginFormController {
     }
 
     private void registerAction(@NotNull final ActionEvent event) {
-        User user = null;
-        if (nameField.getText() != null && passwordField.getText() != null) {
-            user = new User(nameField.getText(), passwordField.getText());
-        }
-        if (user != null){
-           RegistrationManager registrationManager = new RegistrationManager();
-           HashMap<String, Object> answer = registrationManager.onRegistration(user);
-           if (answer.get("status").equals("success")){
-               //loginManager.onLogin();
-           } else if(answer.get("status").equals("failure") && answer.get("message").equals("Name already taken")){
-               Alert alert = new Alert(Alert.AlertType.ERROR);
-               alert.setTitle("Fehler");
-               alert.setHeaderText("Fehler bei der Registrierung");
-               alert.setContentText("Dieser Benutzer ist bereits registriert");
-               alert.showAndWait();
-           }
-       }
+
     }
 
     public void onLogin() {
