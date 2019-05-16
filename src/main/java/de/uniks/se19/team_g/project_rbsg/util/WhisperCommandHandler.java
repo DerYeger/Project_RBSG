8f3f52d58578b302196193707dcb447223f7636a
Jan Müller <janmueller3698@gmail.com>
2019-05-16 12:12:58 +0200

    Added whisper command handler

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/util/WhisperCommandHandler.java
@@ -0,0 +1,33 @@
+package de.uniks.se19.team_g.project_rbsg.util;
+
+import de.uniks.se19.team_g.project_rbsg.controller.ChatController;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+
+/**
+ * @author Jan Müller
+ */
+public class WhisperCommandHandler implements ChatCommandHandler {
+
+    private static final String COMMAND = "w";
+
+    private ChatController chatController;
+
+    public WhisperCommandHandler(@NonNull final ChatController chatController) {
+        this.chatController = chatController;
+    }
+
+
+    //TODO check if user exists?
+    @Override
+    public boolean handleCommand(@NonNull final String command, @Nullable final String[] options) throws Exception {
+        System.out.println("options " + options);
+        if (command.equals(COMMAND)) { //matching command
+            if (options != null && options.length > 0) { //matching options
+                chatController.addPrivateTab(options[0]);
+                return true;
+            }
+        }
+        return false;
+    }
+}
