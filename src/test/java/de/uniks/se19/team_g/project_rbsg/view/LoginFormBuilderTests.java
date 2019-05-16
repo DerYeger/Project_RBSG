ad6d126be68fa7733c75d96f1e0364b1ecb986c0
Juri <juri.lozowoj@freenet.de>
2019-05-16 19:28:32 +0200

    Added: ContextConfiguration Annotation
    Tests still don't run

--- src/test/java/de/uniks/se19/team_g/project_rbsg/view/LoginFormBuilderTests.java
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/view/LoginFormBuilderTests.java
@@ -1,13 +1,21 @@
 package de.uniks.se19.team_g.project_rbsg.view;
 
+import de.uniks.se19.team_g.project_rbsg.JavaConfig;
+import de.uniks.se19.team_g.project_rbsg.apis.RegistrationManager;
+import de.uniks.se19.team_g.project_rbsg.controller.LoginFormController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.testfx.framework.junit.ApplicationTest;
 
 import java.io.IOException;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {JavaConfig.class, RegistrationManager.class, LoginFormController.class, LoginFormBuilder.class})
 public class LoginFormBuilderTests extends ApplicationTest {
 
     @Test
