f7a70e2bd41ea2bf59355de38e2e67262b28dadf
Kea Nu <kestue7@gmail.com>
2019-05-16 02:56:09 +0200

    added unirestException

--- src/test/java/de/uniks/se19/team_g/project_rbsg/apis/LoginMangerTest.java
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/apis/LoginMangerTest.java
@@ -1,5 +1,6 @@
 package de.uniks.se19.team_g.project_rbsg.apis;
 
+import com.mashape.unirest.http.exceptions.UnirestException;
 import de.uniks.se19.team_g.project_rbsg.model.User;
 import org.json.JSONException;
 import org.junit.Assert;
@@ -13,14 +14,12 @@ import java.util.concurrent.ExecutionException;
 public class LoginMangerTest {
 
     @Test
-    public void loginTest() throws ExecutionException, InterruptedException, JSONException {
-
+    public void loginTest() throws ExecutionException, InterruptedException, JSONException, UnirestException {
         User user = new User("MasterChief", "john-117");
         User userClone = LoginManager.onLogin(user);
         Assert.assertNotNull(userClone.getUserKey());
         Assert.assertEquals(user.getName(), userClone.getName());
         Assert.assertNull(userClone.getPassword());
-
     }
 
 }
