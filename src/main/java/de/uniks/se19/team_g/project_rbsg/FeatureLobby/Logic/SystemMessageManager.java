336049def75c99a5f2252a8fe7d60025876e0e64
millenium9977 <38467570+millenium9977@users.noreply.github.com>
2019-05-16 15:52:47 +0200

    Implemented SystemMessageManager

--- src/main/java/de/uniks/se19/team_g/project_rbsg/FeatureLobby/Logic/SystemMessageManager.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/FeatureLobby/Logic/SystemMessageManager.java
@@ -1,5 +1,24 @@
-package de.uniks.se19.team_g.project_rbsg.FeatureLobby.Logic.Contract;
+package de.uniks.se19.team_g.project_rbsg.FeatureLobby.Logic;
 
-public class SystemMessageManager
+import de.uniks.se19.team_g.project_rbsg.FeatureLobby.Logic.Contract.ISystemMessageHandler;
+import de.uniks.se19.team_g.project_rbsg.FeatureLobby.Logic.Contract.IWSCallback;
+
+import java.util.ArrayList;
+
+public class SystemMessageManager implements IWSCallback
 {
+    private ArrayList<ISystemMessageHandler> systemMessageHandlers = new ArrayList<>();
+
+    public SystemMessageManager() {
+
+    }
+
+    @Override
+    public void handle(final String message)
+    {
+        for (ISystemMessageHandler systemMessageHandler : systemMessageHandlers)
+        {
+            systemMessageHandler.handleSystemMessage(message);
+        }
+    }
 }
