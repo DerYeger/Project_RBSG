0ab8da9d59564dd752aa0a347a289ed5e06598cb
millenium9977 <38467570+millenium9977@users.noreply.github.com>
2019-05-16 21:50:04 +0200

    Added LobbyView Builder

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/FeatureLobby/UI/Views/LobbyViewBuilder.java
@@ -0,0 +1,26 @@
+package de.uniks.se19.team_g.project_rbsg.FeatureLobby.UI.Views;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import org.springframework.lang.NonNull;
+
+/**
+ * @author Georg Siebert
+ */
+
+public class LobbyViewBuilder
+{
+    private Node lobbyView;
+
+    public @NonNull Node buildLobbyScene() throws Exception{
+        if(lobbyView == null) {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("LobbyView.fxml"));
+            lobbyView = fxmlLoader.load();
+
+            final LobbyViewController lobbyViewController = fxmlLoader.getController();
+            lobbyViewController.init();
+        }
+        return lobbyView;
+    }
+}
