4fce70d712e3b438bbda2f02775eddda31255b5d
Jan Müller <janmueller3698@gmail.com>
2019-05-16 12:13:30 +0200

    Minor changes

--- src/main/java/de/uniks/se19/team_g/project_rbsg/controller/ChatTabController.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/controller/ChatTabController.java
@@ -3,12 +3,12 @@ package de.uniks.se19.team_g.project_rbsg.controller;
 import javafx.scene.control.Tab;
 import org.springframework.lang.NonNull;
 
+/**
+ * @author Jan Müller
+ */
 public class ChatTabController {
 
-    private Tab chatTab;
-
-    public void init(@NonNull final Tab chatTab, @NonNull final ChatController chatController) {
-        this.chatTab = chatTab;
-        chatTab.setOnCloseRequest(event -> chatController.removeTab(chatTab));
+    public void init(@NonNull final ChatController chatController, @NonNull final Tab chatTab, @NonNull final String channel) {
+        chatTab.setOnCloseRequest(event -> chatController.removeTab(channel));
     }
 }
