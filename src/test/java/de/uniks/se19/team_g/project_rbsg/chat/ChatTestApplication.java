80a4acf875b189ea585ab2cd12a6b4d41dae50b0
Jan Müller <uk046154@student.uni-kassel.de>
2019-05-19 18:14:54 +0200

    Added chat test application

---
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/chat/ChatTestApplication.java
@@ -0,0 +1,11 @@
+package de.uniks.se19.team_g.project_rbsg.chat;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatTestApplication {
+
+    public static void main(String[] args) {
+        ChatTestFXApplication.main(args);
+    }
+}
---
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/chat/ChatTestFXApplication.java
@@ -0,0 +1,62 @@
+package de.uniks.se19.team_g.project_rbsg.chat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.uniks.se19.team_g.project_rbsg.Lobby.Logic.RESTClient;
+import de.uniks.se19.team_g.project_rbsg.Lobby.Logic.WebSocketClient;
+import de.uniks.se19.team_g.project_rbsg.Lobby.Logic.WebSocketConfigurator;
+import de.uniks.se19.team_g.project_rbsg.controller.ChatController;
+import de.uniks.se19.team_g.project_rbsg.controller.ChatWebSocketCallback;
+import de.uniks.se19.team_g.project_rbsg.model.User;
+import de.uniks.se19.team_g.project_rbsg.view.ChatBuilder;
+import javafx.application.Application;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.validation.constraints.NotNull;
+import java.io.IOException;
+
+public class ChatTestFXApplication extends Application {
+
+    private static String userName;
+
+    public static void main(String[] args) {
+        userName = args[0];
+        launch(args);
+    }
+
+    @Override
+    public void start(@NotNull final Stage primaryStage) throws IOException {
+        final RESTClient restClient = new RESTClient();
+        String userKey = "nothing";
+        final String loginResponse = restClient.post("/user/login", null, null, "{ \"name\" : \"" + userName + "\", \"password\" : \"" + userName + "\" }");
+        System.out.println(userName);
+        try
+        {
+            userKey = new ObjectMapper().readTree(loginResponse).get("data").get("userKey").asText();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        WebSocketConfigurator.userKey = userKey;
+        final User user = new User(userName, userName);
+
+        final ChatWebSocketCallback webSocketCallback = new ChatWebSocketCallback();
+        final WebSocketClient webSocketClient = new WebSocketClient("/chat?user=" + userName, webSocketCallback);
+        final ChatController chatController = new ChatController(user, webSocketClient, webSocketCallback);
+
+        final ChatBuilder chatBuilder = new ChatBuilder(chatController);
+        final Node chat = chatBuilder.getChat();
+
+        final Scene scene = new Scene((Parent) chat);
+
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(200);
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+}
