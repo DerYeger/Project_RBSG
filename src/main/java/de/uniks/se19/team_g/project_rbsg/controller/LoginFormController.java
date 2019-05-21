2c160d5ca257e0b18ef4fc6bbddaae458e644783
Jan Müller <janmueller3698@gmail.com>
2019-05-21 15:51:18 +0200

    Implemented scene changing

--- src/main/java/de/uniks/se19/team_g/project_rbsg/controller/LoginFormController.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/controller/LoginFormController.java
@@ -4,6 +4,7 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import de.uniks.se19.team_g.project_rbsg.apis.LoginManager;
 import de.uniks.se19.team_g.project_rbsg.apis.RegistrationManager;
 import de.uniks.se19.team_g.project_rbsg.model.User;
+import de.uniks.se19.team_g.project_rbsg.view.SceneManager;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -11,6 +12,10 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.lang.Nullable;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
@@ -36,6 +41,9 @@ public class LoginFormController {
     private Button registerButton;
 
     private final RegistrationManager registrationManager;
+
+    @Autowired
+    private ApplicationContext context;
 
     public LoginFormController(RegistrationManager registrationManager) {
         this.registrationManager = registrationManager;
@@ -103,12 +111,7 @@ public class LoginFormController {
     }
 
     public void onLogin(@NonNull User user) {
-        // change scene here...
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Platzhalter");
-        alert.setHeaderText("Login erfolgreich");
-        alert.setContentText("Szenenwechsel zur Lobby muss noch implementiert werden");
-        alert.showAndWait();
+        context.getBean(SceneManager.class).setLobbyScene();
     }
 
     public void onRegistration() {
