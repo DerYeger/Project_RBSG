19c27df1ee7c91bbc527f9ecf65585d16d8844b5
Juri <juri.lozowoj@freenet.de>
2019-05-21 20:25:30 +0200

    Added getClientHttpRequestFactory method to JavaConfig.class
    Connection-/ and readTimeOut for RestTemplate is now set to 10 sec

--- src/main/java/de/uniks/se19/team_g/project_rbsg/JavaConfig.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/JavaConfig.java
@@ -1,11 +1,17 @@
 package de.uniks.se19.team_g.project_rbsg;
 
 import javafx.fxml.FXMLLoader;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.BeansException;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -14,7 +20,6 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class JavaConfig implements ApplicationContextAware {
-
 
     private ApplicationContext context;
 
@@ -26,8 +31,21 @@ public class JavaConfig implements ApplicationContextAware {
     }
 
     @Bean
+    public RestTemplateBuilder restTemplateBuilder(){
+        return new RestTemplateBuilder();
+    }
+
+    @Bean
     public RestTemplate restTemplate(){
-        return new RestTemplate();
+        return new RestTemplate(getClientHttpRequestFactory());
+    }
+
+    private ClientHttpRequestFactory getClientHttpRequestFactory() {
+        int timeOut = 10000;
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setConnectTimeout(timeOut);
+        clientHttpRequestFactory.setReadTimeout(timeOut);
+        return clientHttpRequestFactory;
     }
 
     @Override
