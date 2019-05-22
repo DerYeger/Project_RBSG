b16ba52ec653b14c87466fee0e743e7a0869d2d4
millenium9977 <38467570+millenium9977@users.noreply.github.com>
2019-05-22 10:31:04 +0200

    using other user class

--- src/main/java/de/uniks/se19/team_g/project_rbsg/Lobby/Logic/SystemMessageManager.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/Lobby/Logic/SystemMessageManager.java
@@ -1,8 +1,8 @@
 package de.uniks.se19.team_g.project_rbsg.Lobby.Logic;
 
-import de.uniks.se19.team_g.project_rbsg.Lobby.Logic.Contract.DataClasses.User;
 import de.uniks.se19.team_g.project_rbsg.Lobby.Logic.Contract.ISystemMessageHandler;
 import de.uniks.se19.team_g.project_rbsg.Lobby.Logic.Contract.IWebSocketCallback;
+import de.uniks.se19.team_g.project_rbsg.model.User;
 
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
