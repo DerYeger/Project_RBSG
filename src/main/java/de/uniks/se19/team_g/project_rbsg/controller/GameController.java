9fd090b04dd50dd222b3b92959fb81afede6e30b
Juri <juri.lozowoj@freenet.de>
2019-05-22 19:20:04 +0200

    Added GameController and GameControllerTest

--- src/main/java/de/uniks/se19/team_g/project_rbsg/controller/GameController.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/controller/GameController.java
@@ -1,4 +1,44 @@
 package de.uniks.se19.team_g.project_rbsg.controller;
 
+import de.uniks.se19.team_g.project_rbsg.model.Game;
+import de.uniks.se19.team_g.project_rbsg.model.User;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
+
+@Component
 public class GameController {
+
+    final String uri = "https://rbsg.uniks.de/api/game/:id";
+
+    private RestTemplate restTemplate;
+
+    public GameController(@Nullable RestTemplate restTemplate){
+        this.restTemplate = ((restTemplate == null) ? new RestTemplate() : restTemplate);
+    }
+
+    public CompletableFuture joinGame(@NonNull User user, @NonNull Game game){
+        HttpHeaders header = new HttpHeaders();
+        HashMap<String, Object> requestBody = new HashMap<>();
+        header.set("userKey", user.getUserKey());
+
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(uri)
+                .queryParam("id", game.getGameId());
+
+        HttpEntity<?> request = new HttpEntity<Object>(requestBody, header);
+
+        return CompletableFuture.supplyAsync(() -> this.restTemplate.exchange(
+                uriBuilder.toUriString(),
+                HttpMethod.GET,
+                request,
+                HashMap.class));
+    }
 }
--- src/test/java/de/uniks/se19/team_g/project_rbsg/controller/GameControllerTest.java
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/controller/GameControllerTest.java
@@ -1,4 +1,35 @@
 package de.uniks.se19.team_g.project_rbsg.controller;
 
+import de.uniks.se19.team_g.project_rbsg.model.Game;
+import de.uniks.se19.team_g.project_rbsg.model.GameBuilder;
+import de.uniks.se19.team_g.project_rbsg.model.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class GameControllerTest {
+
+    @Test
+    public void joinGameTest(){
+        GameController gameController = new GameController(new RestTemplate());
+        final User testUser = new User("Juri", "geheim");
+        testUser.setUserKey("ad904304-205f-4eb3-8020-4a0bf1244104");
+        final Game testGame = new GameBuilder().getGame("SuperGame", 4);
+        testGame.setGameId("5ce5839750487200013b9b16");
+
+        CompletableFuture<HashMap<String, Object>> joinedGame = gameController.joinGame(testUser, testGame);
+        AtomicReference<String> status = new AtomicReference<>();
+        AtomicReference<String> message = new AtomicReference<>();
+        joinedGame.thenAccept(
+                map -> {
+                    status.set((String) map.get("status"));
+                    message.set((String) map.get("message"));
+                }
+        ).join();
+
+        Assert.assertEquals("success", status.get());
+    }
 }
