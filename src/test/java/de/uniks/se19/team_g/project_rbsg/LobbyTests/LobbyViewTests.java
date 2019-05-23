03d834c8abe23e1a35304a905ca8bdb2c120efe4
millenium9977 <38467570+millenium9977@users.noreply.github.com>
2019-05-23 17:10:40 +0200

    LobbyTest is now testing the player list

--- src/test/java/de/uniks/se19/team_g/project_rbsg/LobbyTests/LobbyViewTests.java
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/LobbyTests/LobbyViewTests.java
@@ -1,24 +1,38 @@
 package de.uniks.se19.team_g.project_rbsg.LobbyTests;
 
 import de.uniks.se19.team_g.project_rbsg.JavaConfig;
+import de.uniks.se19.team_g.project_rbsg.Lobby.CrossCutting.DataClasses.Game;
 import de.uniks.se19.team_g.project_rbsg.Lobby.CrossCutting.DataClasses.Player;
+import de.uniks.se19.team_g.project_rbsg.Lobby.Logic.*;
 import de.uniks.se19.team_g.project_rbsg.Lobby.UI.Views.LobbyViewBuilder;
 import de.uniks.se19.team_g.project_rbsg.Lobby.UI.Views.LobbyViewController;
 import de.uniks.se19.team_g.project_rbsg.apis.RegistrationManager;
+import de.uniks.se19.team_g.project_rbsg.controller.ChatController;
 import de.uniks.se19.team_g.project_rbsg.controller.LoginFormController;
 import de.uniks.se19.team_g.project_rbsg.view.*;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Cell;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
 import org.testfx.framework.junit.ApplicationTest;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 import static org.junit.Assert.*;
 
 /**
@@ -26,47 +40,84 @@ import static org.junit.Assert.*;
  */
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {JavaConfig.class, LoginFormBuilder.class, LoginFormController.class, RegistrationManager.class, SplashImageBuilder.class, LoginSceneBuilder.class, SceneManager.class, LobbySceneBuilder.class, LobbyViewBuilder.class})
+@ContextConfiguration(classes = {
+        JavaConfig.class,
+        LobbyViewBuilder.class,
+        LobbyViewController.class,
+        LobbyViewTests.ContextConfiguration.class,
+        ChatBuilder.class,
+        ChatController.class
+})
 public class LobbyViewTests extends ApplicationTest
 {
-//    private FXMLLoader fxmlLoader;
-//
-//    @Override
-//    public void start (Stage stage) throws Exception {
-//        fxmlLoader = new FXMLLoader(LobbyViewController.class.getResource("LobbyView.fxml"));
-//        Parent mainNode = fxmlLoader.load();
-//        stage.setScene(new Scene(mainNode, 600, 400));
-//        stage.show();
-//        stage.toFront();
-//    }
-//
-//    @Test
-//    public void testView() {
-//        Label lobbyTitle = lookup("#lobbyTitle").query();
-//        ListView<Player>  playerListView = lookup("#lobbyPlayerListView").query();
-//        assertEquals("Advanced WASP Wars", lobbyTitle.getText());
-//        assertNotNull(playerListView);
-//    }
-//
-//    @Test
-//    public void addPlayerToList() {
-//        LobbyViewController lobbyViewController = fxmlLoader.getController();
-//        assertNotNull(lobbyViewController);
-////        lobbyViewController.getViewModel().getPlayerObservableCollection().addAll(new Player("Tobias"), new Player("Juri"), new Player("Omar"), new Player("Jan"), new Player("Keanu"), new Player("Christoph"), new Player("Georg"));
-//        sleep(2000);
-//    }
-//
-//    @Test
-//    public void TestBuilder() {
-//        LobbyViewBuilder lobbyViewBuilder = new LobbyViewBuilder();
-//        try
-//        {
-//            Node node = lobbyViewBuilder.buildLobbyScene();
-//            assertNotNull(node);
-//        }
-//        catch (Exception e)
-//        {
-//            e.printStackTrace();
-//        }
-//    }
+
+    @Autowired
+    ApplicationContext context;
+
+    @Override
+    public void start(final Stage stage) {
+        LobbyViewBuilder lobbyViewBuilder = context.getBean(LobbyViewBuilder.class);
+        final Scene scene = new Scene((Parent) lobbyViewBuilder.buildLobbyScene());
+
+        stage.setScene(scene);
+        stage.show();
+        stage.toFront();
+    }
+
+    @TestConfiguration
+    static class ContextConfiguration {
+        @Bean
+        public GameManager gameManager() {
+            return new GameManager(new RESTClient(new RestTemplate())) {
+                @Override
+                public Collection<Game> getGames() {
+                    ArrayList<Game> games = new ArrayList<>();
+                    games.add(new Game("1", "GameOfHello", 4, 2 ));
+                    games.add(new Game("2", "Defence of the Ancient", 10, 7));
+                    return games;
+                }
+            };
+        }
+
+        @Bean
+        public PlayerManager playerManager() {
+            return new PlayerManager(new RESTClient(new RestTemplate())) {
+                @Override
+                public Collection<Player> getPlayers() {
+                    ArrayList<Player> players = new ArrayList<>();
+                    players.add(new Player("Hello"));
+                    players.add(new Player("MOBAHero42"));
+                    return players;
+                }
+            };
+        }
+
+        @Bean
+        public SystemMessageManager systemMessageManager() {
+            return new SystemMessageManager(new WebSocketFactory()) {
+                @Override
+                public void startSocket() {
+
+                }
+            };
+        }
+    }
+
+    @Test
+    public void checkPlayerList() {
+        ListView<Player> playerListView = lookup("#lobbyPlayerListView").query();
+        assertNotNull(playerListView);
+        ObservableList<Player> players = playerListView.getItems();
+        assertEquals(2, players.size());
+        Cell<Player> cellHello = lookup("#playerCellHello").query();
+        Cell<Player> cellMobaHero = lookup("#playerCellMOBAHero42").query();
+        assertNotNull(cellHello);
+        assertNotNull(cellMobaHero);
+        clickOn("#playerCellHello");
+        clickOn("#playerCellMOBAHero42");
+
+        assertEquals("Hello", cellHello.getItem().getName());
+        assertEquals("MOBAHero42", cellMobaHero.getItem().getName());
+    }
+
 }
