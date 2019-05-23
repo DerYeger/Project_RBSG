21517f7be6aee769d5bdb90c2dfdee6997a69abc
millenium9977 <38467570+millenium9977@users.noreply.github.com>
2019-05-23 14:31:08 +0200

    added startWebSocket Method to controll whenn the web socket is started WebSocket is now generated over a WebSocketFactory

--- src/main/java/de/uniks/se19/team_g/project_rbsg/Lobby/Logic/SystemMessageManager.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/Lobby/Logic/SystemMessageManager.java
@@ -4,6 +4,7 @@ import de.uniks.se19.team_g.project_rbsg.Lobby.Logic.Contract.ISystemMessageHand
 import de.uniks.se19.team_g.project_rbsg.Lobby.Logic.Contract.IWebSocketCallback;
 import de.uniks.se19.team_g.project_rbsg.Lobby.Logic.SystemMessageHandler.DefaultSystemMessageHandler;
 import de.uniks.se19.team_g.project_rbsg.model.User;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.NotNull;
@@ -22,12 +23,19 @@ public class SystemMessageManager implements IWebSocketCallback
     private ArrayList<ISystemMessageHandler> systemMessageHandlers;
     private WebSocketClient webSocketClient;
 
-    public SystemMessageManager(final DefaultSystemMessageHandler defaultSystemMessageHandler, WebSocketClient webSocketClient) {
+    public SystemMessageManager(WebSocketFactory webSocketFactory) {
         systemMessageHandlers = new ArrayList<>();
-        systemMessageHandlers.add(defaultSystemMessageHandler);
+        systemMessageHandlers.add(new DefaultSystemMessageHandler());
 
-        this.webSocketClient = webSocketClient;
-        webSocketClient.start(endpoint, this);
+        this.webSocketClient = webSocketFactory.getSocket("/system", this);
+    }
+
+    public void startSocket() {
+        webSocketClient.start();
+    }
+
+    public void addMessageHandler(final @NonNull ISystemMessageHandler messageHandler) {
+        systemMessageHandlers.add(messageHandler);
     }
 
     @Override
