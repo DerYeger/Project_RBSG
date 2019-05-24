e950dfa3c51fe54ee96bdf3cd5ceb4f659b768b2
millenium9977 <38467570+millenium9977@users.noreply.github.com>
2019-05-24 02:01:51 +0200

    extended Lobby DataClass

--- src/main/java/de/uniks/se19/team_g/project_rbsg/Lobby/CrossCutting/DataClasses/Lobby.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/Lobby/CrossCutting/DataClasses/Lobby.java
@@ -1,10 +1,11 @@
 package de.uniks.se19.team_g.project_rbsg.Lobby.CrossCutting.DataClasses;
 
 import de.uniks.se19.team_g.project_rbsg.Lobby.Logic.SystemMessageManager;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+
+import java.util.Collection;
 
 public class Lobby
 {
@@ -12,20 +13,21 @@ public class Lobby
     private ObservableList<Game> games;
     private ObservableList<Player> players;
 
-    public SystemMessageManager getSystemMessageManager() {
-        return systemMessageManager;
-    }
-
-    public void setSystemMessageManager(SystemMessageManager systemMessageManager) {
-        this.systemMessageManager = systemMessageManager;
-    }
-
     public Lobby()
     {
         games = FXCollections.observableArrayList();
         players = FXCollections.observableArrayList();
     }
 
+    public SystemMessageManager getSystemMessageManager()
+    {
+        return systemMessageManager;
+    }
+
+    public void setSystemMessageManager(SystemMessageManager systemMessageManager)
+    {
+        this.systemMessageManager = systemMessageManager;
+    }
 
     public ObservableList<Game> getGames()
     {
@@ -37,6 +39,111 @@ public class Lobby
         this.games = games;
     }
 
+    public void addGame(Game game)
+    {
+        if (game != null)
+        {
+            Platform.runLater(() -> games.add(game));
+        }
+    }
+
+    public Lobby withGame(Game game)
+    {
+        addGame(game);
+        return this;
+    }
+
+    public void addGames(Game... gameList)
+    {
+        if (gameList != null)
+        {
+            for (Game game : gameList)
+            {
+                if (game != null)
+                {
+                    Platform.runLater(() -> games.add(game));
+                }
+            }
+        }
+    }
+
+    public Lobby withGames(Game... gameList)
+    {
+        addGames(gameList);
+        return this;
+    }
+
+    public void addAllGames(Collection<Game> gamesList)
+    {
+        if(games != null) {
+            for (Game game: gamesList)
+            {
+                Platform.runLater(() -> games.add(game));
+            }
+        }
+    }
+
+    public void removeGame(Game game)
+    {
+        if (game != null && games.contains(game))
+        {
+            Platform.runLater(() ->games.remove(game));
+        }
+    }
+
+    public void addPlayer(Player player)
+    {
+        if (player != null)
+        {
+            Platform.runLater(() -> players.add(player));
+        }
+    }
+
+    public Lobby withPlayer(Player player)
+    {
+        addPlayer(player);
+        return this;
+    }
+
+    public void addPlayers(Player... playerList)
+    {
+        if (playerList != null)
+        {
+            for (Player player : playerList)
+            {
+                if (player != null)
+                {
+                    Platform.runLater(() -> players.add(player));
+                }
+            }
+        }
+    }
+
+    public Lobby withPlayers(Player... playerList)
+    {
+        addPlayers(playerList);
+        return this;
+    }
+
+    public void addAllPlayer(Collection<Player> playerList)
+    {
+        if(playerList != null) {
+            for (Player player: playerList)
+            {
+                Platform.runLater(() -> players.add(player));
+            }
+        }
+    }
+
+    public void removePlayer(Player player)
+    {
+        if (player != null && players.contains(player))
+        {
+            Platform.runLater(() -> players.remove(player));
+        }
+    }
+
+
     public ObservableList<Player> getPlayers()
     {
         return players;
@@ -46,4 +153,5 @@ public class Lobby
     {
         this.players = players;
     }
+
 }
