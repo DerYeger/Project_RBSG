5713ac7ddf37e1e4dac97ce3d233299b71bae27b
millenium9977 <38467570+millenium9977@users.noreply.github.com>
2019-05-24 11:00:46 +0200

    Added userJoinedMessageHandler + Test

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/Lobby/Logic/SystemMessageHandler/UserJoinedMessageHandler.java
@@ -0,0 +1,66 @@
+package de.uniks.se19.team_g.project_rbsg.Lobby.Logic.SystemMessageHandler;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.uniks.se19.team_g.project_rbsg.Lobby.CrossCutting.DataClasses.Lobby;
+import de.uniks.se19.team_g.project_rbsg.Lobby.CrossCutting.DataClasses.Player;
+import de.uniks.se19.team_g.project_rbsg.Lobby.Logic.Contract.ISystemMessageHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class UserJoinedMessageHandler implements ISystemMessageHandler
+{
+    private Logger logger = LoggerFactory.getLogger(UserJoinedMessageHandler.class);
+
+    private Lobby lobby;
+    private ObjectMapper objectMapper;
+
+    public UserJoinedMessageHandler()
+    {
+        objectMapper = new ObjectMapper();
+    }
+
+    public void setLobby(Lobby lobby)
+    {
+        this.lobby = lobby;
+    }
+
+    @Override
+    public void handleSystemMessage(final @NonNull String message)
+    {
+        JsonNode messageNode = null;
+
+        try
+        {
+            messageNode = objectMapper.readTree(message);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        if (messageNode == null)
+        {
+            return;
+        }
+        if (!messageNode.get("action").asText().equals("userJoined"))
+        {
+            return;
+        }
+
+        String name = messageNode.get("data").get("name").asText();
+        if (lobby != null)
+        {
+            lobby.addPlayer(new Player(name));
+        }
+        else
+        {
+            logger.warn("Lobby is null");
+        }
+    }
+}
--- src/test/java/de/uniks/se19/team_g/project_rbsg/LobbyTests/SystemMessageHandlerTest.java
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/LobbyTests/SystemMessageHandlerTest.java
@@ -1,28 +1,53 @@
 package de.uniks.se19.team_g.project_rbsg.LobbyTests;
 
 import de.uniks.se19.team_g.project_rbsg.JavaConfig;
+import de.uniks.se19.team_g.project_rbsg.Lobby.CrossCutting.DataClasses.Lobby;
 import de.uniks.se19.team_g.project_rbsg.Lobby.Logic.SystemMessageHandler.DefaultSystemMessageHandler;
+import de.uniks.se19.team_g.project_rbsg.Lobby.Logic.SystemMessageHandler.UserJoinedMessageHandler;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.testfx.framework.junit.ApplicationTest;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {
         JavaConfig.class,
-        DefaultSystemMessageHandler.class
+        DefaultSystemMessageHandler.class,
+        UserJoinedMessageHandler.class
 })
-public class SystemMessageHandlerTest
+public class SystemMessageHandlerTest extends ApplicationTest
 {
+    private final String userJoinedMessage = "{\"action\":\"userJoined\",\"data\":{\"name\":\"hello2\"}}";
+
     @Autowired
     private ApplicationContext context;
 
     @Test
     public void testDefaultSystemMessageHandler() {
         DefaultSystemMessageHandler defaultHandler = context.getBean(DefaultSystemMessageHandler.class);
+
         defaultHandler.handleSystemMessage("Hallo");
     }
 
+    @Test
+    public void testUserJoinedMessageHandler() {
+        UserJoinedMessageHandler userJoinedMessageHandler = context.getBean(UserJoinedMessageHandler.class);
+
+        Lobby lobby = new Lobby();
+        userJoinedMessageHandler.setLobby(lobby);
+
+        assertNotNull(lobby.getPlayers());
+
+        userJoinedMessageHandler.handleSystemMessage(userJoinedMessage);
+
+        sleep(500);
+
+        assertEquals(1, lobby.getPlayers().size());
+        assertEquals("hello2" , lobby.getPlayers().get(0).getName());
+    }
 }
