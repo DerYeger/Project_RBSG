8ce0e1aedbf5a2510e25cb0252f214865d162960
Kea Nu <kestue7@gmail.com>
2019-05-26 01:45:34 +0200

    created LeaveGame

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/Ingame/LeaveGame.java
@@ -0,0 +1,39 @@
+package de.uniks.se19.team_g.project_rbsg.Ingame;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.IOException;
+
+public class LeaveGame {
+
+    @FXML
+    private Label leaveGameLabel;
+
+    @FXML
+    private ImageView leaveGameImage;
+
+    private FXMLLoader fxmlLoader;
+    private Node leaveGameView;
+
+    public Node buildleaveGame(){
+        if(fxmlLoader == null) {
+            fxmlLoader = new FXMLLoader(getClass().getResource("leaveGame.fxml"));
+            fxmlLoader.setController(this);
+            try {
+                leaveGameView = fxmlLoader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        leaveGameLabel.setText("Leave game");
+        Image image = new Image(String.valueOf(getClass().getResource("Images/x-button.png")));
+        leaveGameImage.setImage(image);
+        return leaveGameView;
+    }
+
+}
