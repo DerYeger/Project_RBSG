3e7ad9abc27adbefe36033e9238718f14ad7dda2
Kea Nu <kestue7@gmail.com>
2019-05-25 21:26:29 +0200

    created ingame view builder

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/Ingame/IngameViewBuilder.java
@@ -0,0 +1,31 @@
+package de.uniks.se19.team_g.project_rbsg.Ingame;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IngameViewBuilder {
+
+    private Node ingameView;
+
+    private FXMLLoader fxmlLoader;
+
+    @Autowired
+    public IngameViewBuilder(FXMLLoader fxmlLoader) {
+        this.fxmlLoader = fxmlLoader;
+    }
+
+    public @NonNull
+    Node buildIngameScene() throws Exception{
+        if(ingameView == null) {
+            fxmlLoader.setLocation(getClass().getResource("ingame-view.fxml"));
+            ingameView = fxmlLoader.load();
+            final IngameViewController ingameViewController = fxmlLoader.getController();
+            ingameViewController.init();
+        }
+        return ingameView;
+    }
+}
