2650273355ff9afb85c734c23398033164a60ce8
Jan Müller <janmueller3698@gmail.com>
2019-05-30 20:26:53 +0200

    Added additional tests
    Feature should now have 100% coverage

--- src/test/java/de/uniks/se19/team_g/project_rbsg/server/rest/LogoutManagerTests.java
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/server/rest/LogoutManagerTests.java
@@ -29,10 +29,105 @@ public class LogoutManagerTests {
                 .setName("TestUser")
                 .setUserKey("1234");
 
+        WebSocketConfigurator.userKey = user.getUserKey();
+
         logoutManager.logout();
 
         Assert.assertNotEquals(user, userProvider.get());
 
         Assert.assertNull(WebSocketConfigurator.userKey);
+    }
+
+    @Test
+    public void testUnsuccessfulLogout() {
+        final UserProvider userProvider = new UserProvider();
+        final RESTClient restClient = new RESTClient(new RestTemplate()) {
+            @Override
+            public String get(final @NonNull String endpoint, final @Nullable MultiValueMap<String, String> headers) {
+                return "{\"status\":\"failure\"}"; //specific status doesn't matter, as long as it isn't "success"
+            }
+        };
+        final LogoutManager logoutManager = new LogoutManager(userProvider, restClient);
+
+        final User user = new User();
+
+        userProvider.set(user).get()
+                .setName("TestUser")
+                .setUserKey("1234");
+
+        WebSocketConfigurator.userKey = user.getUserKey();
+
+        logoutManager.logout();
+
+        Assert.assertEquals(user, userProvider.get());
+
+        Assert.assertEquals("1234", WebSocketConfigurator.userKey);
+    }
+
+    @Test
+    public void testUnknownResponse() {
+        final UserProvider userProvider = new UserProvider();
+        final RESTClient restClient = new RESTClient(new RestTemplate()) {
+            @Override
+            public String get(final @NonNull String endpoint, final @Nullable MultiValueMap<String, String> headers) {
+                return "{ }"; //not containing field "status"
+            }
+        };
+        final LogoutManager logoutManager = new LogoutManager(userProvider, restClient);
+
+        final User user = new User();
+
+        userProvider.set(user).get()
+                .setName("TestUser")
+                .setUserKey("1234");
+
+        WebSocketConfigurator.userKey = user.getUserKey();
+
+        logoutManager.logout();
+
+        Assert.assertEquals(user, userProvider.get());
+
+        Assert.assertEquals("1234", WebSocketConfigurator.userKey);
+    }
+
+    @Test
+    public void testErrorParsing() {
+        final UserProvider userProvider = new UserProvider();
+        final RESTClient restClient = new RESTClient(new RestTemplate()) {
+            @Override
+            public String get(final @NonNull String endpoint, final @Nullable MultiValueMap<String, String> headers) {
+                return "some gibberish";
+            }
+        };
+        final LogoutManager logoutManager = new LogoutManager(userProvider, restClient);
+
+        final User user = new User();
+
+        userProvider.set(user).get()
+                .setName("TestUser")
+                .setUserKey("1234");
+
+        WebSocketConfigurator.userKey = user.getUserKey();
+
+        logoutManager.logout();
+
+        Assert.assertEquals(user, userProvider.get());
+
+        Assert.assertEquals("1234", WebSocketConfigurator.userKey);
+    }
+
+    @Test
+    public void testNoUserLoggedInLogout() {
+        final UserProvider userProvider = new UserProvider();
+        final RESTClient restClient = new RESTClient(new RestTemplate());
+        final LogoutManager logoutManager = new LogoutManager(userProvider, restClient);
+
+        final User emptyUser = userProvider.get();
+
+        WebSocketConfigurator.userKey = null;
+
+        logoutManager.logout();
+
+        Assert.assertEquals(emptyUser, userProvider.get());
     }
 }
