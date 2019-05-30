fd0a413dbb3fe5e830ed1d6f88bc1571b30336c9
Jan Müller <janmueller3698@gmail.com>
2019-05-30 19:22:53 +0200

    Major improvements to logout

--- src/main/java/de/uniks/se19/team_g/project_rbsg/server/rest/LogoutManager.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/server/rest/LogoutManager.java
@@ -1,12 +1,17 @@
 package de.uniks.se19.team_g.project_rbsg.server.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import de.uniks.se19.team_g.project_rbsg.model.UserProvider;
+import de.uniks.se19.team_g.project_rbsg.server.websocket.WebSocketConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
+
+import java.io.IOException;
 
 /**
  * @author Jan Müller
@@ -33,11 +38,31 @@ public class LogoutManager implements ILogoutManager {
             final LinkedMultiValueMap<String, String> headers  = new LinkedMultiValueMap<>();
             headers.add("userKey", userProvider.get().getUserKey());
 
-            restClient.get(ENDPOINT, headers);
+            final String response = restClient.get(ENDPOINT, headers);
 
-            logger.debug("User logged out");
+            if (logoutSuccessful(response)) {
+                logger.debug("Logout successful");
+                userProvider.clear();
+                WebSocketConfigurator.userKey = null;
+            } else {
+                logger.debug("Logout unsuccessful");
+            }
         } else {
             logger.debug("No user logged in");
         }
+    }
+
+    private boolean logoutSuccessful(@NonNull final String response) {
+        try {
+            final ObjectNode json = new ObjectMapper().readValue(response, ObjectNode.class);
+            if (json.has("status")) {
+                return json.get("status").asText().equals("success");
+            } else {
+                logger.debug("Unknown server response");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
