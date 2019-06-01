632af10bde3e493535c9ea0cbb7b2f29176b2248
Kea Nu <kestue7@gmail.com>
2019-06-01 02:22:26 +0200

    added ingame view test

---
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/ingame/IngameViewTests.java
@@ -0,0 +1,69 @@
+package de.uniks.se19.team_g.project_rbsg.ingame;
+
+import de.uniks.se19.team_g.project_rbsg.SceneManager;
+import de.uniks.se19.team_g.project_rbsg.configuration.JavaConfig;
+import de.uniks.se19.team_g.project_rbsg.lobby.core.LobbySceneBuilder;
+import de.uniks.se19.team_g.project_rbsg.lobby.core.ui.LobbyViewBuilder;
+import de.uniks.se19.team_g.project_rbsg.login.LoginFormBuilder;
+import de.uniks.se19.team_g.project_rbsg.login.LoginFormController;
+import de.uniks.se19.team_g.project_rbsg.login.LoginSceneBuilder;
+import de.uniks.se19.team_g.project_rbsg.login.SplashImageBuilder;
+import de.uniks.se19.team_g.project_rbsg.model.Game;
+import de.uniks.se19.team_g.project_rbsg.model.GameProvider;
+import de.uniks.se19.team_g.project_rbsg.model.UserProvider;
+import de.uniks.se19.team_g.project_rbsg.server.rest.LoginManager;
+import de.uniks.se19.team_g.project_rbsg.server.rest.RegistrationManager;
+import javafx.scene.Node;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.testfx.framework.junit.ApplicationTest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {
+        JavaConfig.class,
+        RegistrationManager.class,
+        LoginFormController.class,
+        LoginFormBuilder.class,
+        SplashImageBuilder.class,
+        LoginSceneBuilder.class,
+        SceneManager.class,
+        LobbySceneBuilder.class,
+        LobbyViewBuilder.class,
+        LoginManager.class,
+        IngameSceneBuilder.class,
+        IngameViewBuilder.class,
+        IngameViewController.class,
+        UserProvider.class,
+        IngameViewTests.ContextConfiguration.class
+})
+public class IngameViewTests extends ApplicationTest {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @TestConfiguration
+    static class ContextConfiguration {
+        @Bean
+        public GameProvider gameProvider() {
+            return new GameProvider() {
+                @Override
+                public Game get() {
+                    return new Game("id", "testGame", 4, 1);
+                }
+            };
+        }
+    }
+
+    @Test
+    public void testBuildIngameView() throws Exception {
+        final Node ingameView = context.getBean(IngameViewBuilder.class).buildIngameView();
+        Assert.assertNotNull(ingameView);
+    }
+}
