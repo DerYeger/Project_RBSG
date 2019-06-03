2b86f1544e1a1f3c60349ec2ee584d700757e40a
Kea Nu <kestue7@gmail.com>
2019-06-03 11:26:26 +0200

    placed anchorPane from SceneBuilder into fxml

--- src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/IngameSceneBuilder.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/IngameSceneBuilder.java
@@ -1,6 +1,7 @@
 package de.uniks.se19.team_g.project_rbsg.ingame;
 
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,12 +23,7 @@ public class IngameSceneBuilder {
     public Scene getIngameScene() throws Exception {
         if (ingameScene == null) {
             Node ingameNode = ingameViewBuilder.buildIngameView();
-            final AnchorPane parent = new AnchorPane(ingameNode);
-            AnchorPane.setBottomAnchor(ingameNode, 0.0);
-            AnchorPane.setLeftAnchor(ingameNode, 0.0);
-            AnchorPane.setRightAnchor(ingameNode, 0.0);
-            AnchorPane.setTopAnchor(ingameNode, 0.0);
-            ingameScene = new Scene(parent);
+            ingameScene = new Scene((Parent) ingameNode);
         }
         return ingameScene;
     }
