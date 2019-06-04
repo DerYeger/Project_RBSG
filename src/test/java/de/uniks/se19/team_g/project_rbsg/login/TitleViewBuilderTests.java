2f262f471ea84183dc97c7ab23888724fb1bc183
Jan Müller <janmueller3698@gmail.com>
2019-06-04 17:32:09 +0200

    Fixed merge conflicts

--- src/test/java/de/uniks/se19/team_g/project_rbsg/login/TitleViewBuilderTests.java
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/login/TitleViewBuilderTests.java
@@ -1,12 +1,17 @@
 package de.uniks.se19.team_g.project_rbsg.login;
 
-import de.uniks.se19.team_g.project_rbsg.configuration.JavaConfig;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.testfx.framework.junit.ApplicationTest;
@@ -19,11 +24,32 @@ import java.io.IOException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {
-        JavaConfig.class,
+        TitleViewBuilderTests.ContextConfiguration.class,
         TitleViewBuilder.class,
         TitleViewController.class
 })
 public class TitleViewBuilderTests extends ApplicationTest {
+
+    @TestConfiguration
+    static class ContextConfiguration implements ApplicationContextAware {
+
+        private ApplicationContext context;
+
+        @Bean
+        @Scope("prototype")
+        public FXMLLoader fxmlLoader()
+        {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setControllerFactory(this.context::getBean);
+            return fxmlLoader;
+        }
+
+        @Override
+        public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+            this.context = applicationContext;
+        }
+    }
 
     @Autowired
     private ApplicationContext context;
