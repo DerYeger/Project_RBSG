8601b5b7f0beaaff36738e6bc3f91c20cc1c22b6
Jan Müller <janmueller3698@gmail.com>
2019-06-08 01:45:39 +0200

    Revert "revert merge of 'task/Use-HTTP-status-codes' into develop"
    This reverts commit a5ba932433ad58da196663c5c8c3e9f197420660.

--- src/test/java/de/uniks/se19/team_g/project_rbsg/login/LoginFormControllerTestInvalidCredentialsError.java
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/login/LoginFormControllerTestInvalidCredentialsError.java
@@ -1,7 +1,8 @@
 package de.uniks.se19.team_g.project_rbsg.login;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import de.uniks.se19.team_g.project_rbsg.SceneManager;
-import de.uniks.se19.team_g.project_rbsg.model.User;
 import de.uniks.se19.team_g.project_rbsg.model.UserProvider;
 import de.uniks.se19.team_g.project_rbsg.server.rest.LoginManager;
 import de.uniks.se19.team_g.project_rbsg.server.rest.RegistrationManager;
@@ -25,6 +26,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.test.annotation.DirtiesContext;
@@ -35,13 +40,11 @@ import org.springframework.web.client.RestTemplate;
 import org.testfx.framework.junit.ApplicationTest;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author Keanu Stückrad
+ * @author Jan Müller
  */
-
 @RunWith(SpringJUnit4ClassRunner .class)
 @ContextConfiguration(classes = {
         LoginFormController.class,
@@ -81,14 +84,17 @@ public class LoginFormControllerTestInvalidCredentialsError extends ApplicationT
         public LoginManager loginManager() {
             return new LoginManager(new RestTemplate() {
                 @Override
-                public <T> T postForObject(String url, @Nullable Object request, Class<T> responseType, Object... uriVariables) throws RestClientException {
-                    Assert.assertTrue(request instanceof User);
+                public <T> ResponseEntity<T> exchange(String url, HttpMethod method, @Nullable HttpEntity<?> requestEntity, Class<T> responseType, Object... uriVariables) throws RestClientException {
                     Assert.assertEquals(url, "https://rbsg.uniks.de/api/user/login");
-                    Map<String, Object> testAnswer = new HashMap<>();
-                    testAnswer.put("status", "failure");
-                    testAnswer.put("message", "Invalid Credentials");
-                    testAnswer.put("data", null);
-                    return (T) testAnswer;
+                    final ObjectNode body = new ObjectMapper().createObjectNode();
+                    body.put("status", "failure");
+                    body.put("message", "Invalid Credentials");
+                    body.with("data");
+
+                    ResponseEntity<ObjectNode> response = new ResponseEntity<>(body, HttpStatus.OK);
+                    @SuppressWarnings("unchecked")
+                    ResponseEntity<T> castedResponse = (ResponseEntity<T>) response;
+                    return castedResponse;
                 }
             });
         }
@@ -96,14 +102,17 @@ public class LoginFormControllerTestInvalidCredentialsError extends ApplicationT
         public RegistrationManager registrationManager() {
             return new RegistrationManager(new RestTemplate() {
                 @Override
-                public <T> T postForObject(String url, @Nullable Object request, Class<T> responseType, Object... uriVariables) throws RestClientException {
-                    Assert.assertTrue(request instanceof User);
+                public <T> ResponseEntity<T> exchange(String url, HttpMethod method, @Nullable HttpEntity<?> requestEntity, Class<T> responseType, Object... uriVariables) throws RestClientException {
                     Assert.assertEquals(url, "https://rbsg.uniks.de/api/user");
-                    Map<String, Object> testAnswer = new HashMap<>();
-                    testAnswer.put("status", "failure");
-                    testAnswer.put("message", "Name already taken");
-                    testAnswer.put("data", null);
-                    return (T) testAnswer;
+                    final ObjectNode body = new ObjectMapper().createObjectNode();
+                    body.put("status", "failure");
+                    body.put("message", "Name already taken");
+                    body.with("data");
+
+                    ResponseEntity<ObjectNode> response = new ResponseEntity<>(body, HttpStatus.OK);
+                    @SuppressWarnings("unchecked")
+                    ResponseEntity<T> castedResponse = (ResponseEntity<T>) response;
+                    return castedResponse;
                 }
             });
         }
