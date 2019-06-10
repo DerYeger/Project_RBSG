52ba86a9c95abaf9f44f4f69037c9f3e085fb1da
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-10 18:12:04 +0200

    added basic army builder controller

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/SceneController.java
@@ -0,0 +1,21 @@
+package de.uniks.se19.team_g.project_rbsg.army_builder;
+
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+public class SceneController {
+
+
+    public VBox sideBarLeft;
+    public VBox content;
+    public HBox topContentContainer;
+    public ScrollPane unitListView;
+    public Pane unitDetailView;
+    public VBox armyView;
+    public VBox sideBarRight;
+    public HBox armyBuilderScene;
+
+
+}
