ee5cf145af0d021ef50d457259ba196a81bf0337
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-10 20:49:09 +0200

    added army builder model

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/model/Army.java
@@ -0,0 +1,9 @@
+package de.uniks.se19.team_g.project_rbsg.army_builder.model;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class Army {
+
+    public final ObservableList<Unit> units = FXCollections.observableArrayList();
+}
