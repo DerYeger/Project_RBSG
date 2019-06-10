a9f95c2fb8760209d05f42177d1ea0fa3d325664
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-10 18:50:48 +0200

    Introduced ViewComponent to encapsulate root and controller

--- src/main/java/de/uniks/se19/team_g/project_rbsg/ViewComponent.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/ViewComponent.java
@@ -1,30 +1,30 @@
 package de.uniks.se19.team_g.project_rbsg;
 
 import javafx.fxml.FXMLLoader;
-import org.w3c.dom.Node;
+import javafx.scene.Node;
 
 import java.io.IOException;
 
-public class ViewComponent {
+public class ViewComponent<T> {
     private final Node root;
 
     public Node getRoot() {
         return root;
     }
 
-    public Object getController() {
+    public T getController() {
         return controller;
     }
 
-    private final Object controller;
+    private final T controller;
 
-    public ViewComponent(Node root, Object controller) {
+    public ViewComponent(Node root, T controller) {
 
         this.root = root;
         this.controller = controller;
     }
 
-    public static ViewComponent fromLoader(FXMLLoader loader)
+    public static <T> ViewComponent<T> fromLoader(FXMLLoader loader)
     {
         try {
             loader.load();
@@ -32,6 +32,6 @@ public class ViewComponent {
             throw new RuntimeException(e);
         }
 
-        return new ViewComponent(loader.getRoot(), loader.getController());
+        return new ViewComponent<>(loader.getRoot(), loader.getController());
     }
 }
