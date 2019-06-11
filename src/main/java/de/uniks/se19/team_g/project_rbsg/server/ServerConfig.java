cc0034dde55dfc5a9eac021f26d1087d238295af
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-12 00:46:07 +0200

    moved and improved RestTemplateConfig

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/server/ServerConfig.java
@@ -0,0 +1,31 @@
+package de.uniks.se19.team_g.project_rbsg.server;
+
+import de.uniks.se19.team_g.project_rbsg.server.rest.config.UserKeyInterceptor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@Configuration
+public class ServerConfig {
+
+    public static final int TIMEOUT_CONNECT = 10;
+    public static final int TIMEOUT_READ = 15;
+
+    @Bean
+    public RestTemplate rbsgTemplate(
+        UserKeyInterceptor userKeyInterceptor
+    ) {
+
+        return new RestTemplateBuilder()
+                .additionalInterceptors(userKeyInterceptor)
+                .rootUri("https://rbsg.uniks.de/api")
+                .setConnectTimeout(Duration.ofSeconds(TIMEOUT_CONNECT))
+                .setReadTimeout(Duration.ofMillis(TIMEOUT_READ))
+                .build();
+    }
+}
