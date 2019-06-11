19b90bf210b92db78943ccd3cea51ac27b4fe21b
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-11 22:08:55 +0200

    changed scroll pane placeholder to ListView in ArmyBuilderScene

--- src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/SceneController.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/SceneController.java
@@ -3,7 +3,7 @@ package de.uniks.se19.team_g.project_rbsg.army_builder;
 import de.uniks.se19.team_g.project_rbsg.ViewComponent;
 import de.uniks.se19.team_g.project_rbsg.army_builder.unit_detail.UnitDetailController;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -24,7 +24,7 @@ public class SceneController implements Initializable {
     public VBox sideBarLeft;
     public VBox content;
     public HBox topContentContainer;
-    public ScrollPane unitListView;
+    public ListView unitListView;
     public Pane unitDetailView;
     public VBox armyView;
     public VBox sideBarRight;
