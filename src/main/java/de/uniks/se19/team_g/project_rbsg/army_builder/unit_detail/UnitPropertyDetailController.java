42806c5cb495df57bd8f462da26edc96acabdce2
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-11 18:15:35 +0200

    implemented UnitPropertyDetailController to bind number, icon pending

--- src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/unit_detail/UnitPropertyDetailController.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/unit_detail/UnitPropertyDetailController.java
@@ -1,9 +1,18 @@
 package de.uniks.se19.team_g.project_rbsg.army_builder.unit_detail;
 
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.util.converter.NumberStringConverter;
 
 public class UnitPropertyDetailController {
+    private NumberStringConverter converter = new NumberStringConverter();
+
     public Label propertyValue;
     public VBox propertyIcon;
+
+
+    public void bindTo(SimpleIntegerProperty prop) {
+        propertyValue.textProperty().bindBidirectional(prop, converter);
+    }
 }
