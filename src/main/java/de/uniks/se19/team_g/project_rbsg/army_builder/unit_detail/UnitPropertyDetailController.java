c018eb4864e8a120059d5776ed2ca73f22975b34
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-11 17:38:48 +0200

    added UnitPropertyDetailController

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/unit_detail/UnitPropertyDetailController.java
@@ -0,0 +1,9 @@
+package de.uniks.se19.team_g.project_rbsg.army_builder.unit_detail;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class UnitPropertyDetailController {
+    public Label propertyValue;
+    public VBox propertyIcon;
+}
