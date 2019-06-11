1fdf27f2f1d4c6d853b5ff8538f4ddfedfcc6f5c
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-12 00:47:42 +0200

    optimized imports

--- src/main/java/de/uniks/se19/team_g/project_rbsg/server/ServerConfig.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/server/ServerConfig.java
@@ -4,8 +4,6 @@ import de.uniks.se19.team_g.project_rbsg.server.rest.config.UserKeyInterceptor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
