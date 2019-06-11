81b1d96c2cb28f5caf24a0b62eb5650a0564b3d8
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-12 00:47:33 +0200

    Added GetUnitTypesService

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/server/rest/army/GetUnitTypesService.java
@@ -0,0 +1,32 @@
+package de.uniks.se19.team_g.project_rbsg.server.rest.army;
+
+import de.uniks.se19.team_g.project_rbsg.server.rest.RBSGDataResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+
+@Component
+public class GetUnitTypesService {
+
+    final RestTemplate rbsgTemplate;
+
+
+    public GetUnitTypesService(RestTemplate rbsgTemplate) {
+        this.rbsgTemplate = rbsgTemplate;
+    }
+
+    public CompletableFuture<List<UnitType>> queryUnitTypes()
+    {
+        return CompletableFuture.supplyAsync(this::doQueryUnitTypes);
+    }
+
+    private List<UnitType> doQueryUnitTypes() {
+        final ResponseType response = rbsgTemplate.getForObject("/army/units", ResponseType.class);
+        return Objects.requireNonNull(response).data;
+    }
+
+    private class ResponseType extends RBSGDataResponse<List<UnitType>> {}
+}
---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/server/rest/army/UnitType.java
@@ -0,0 +1,12 @@
+package de.uniks.se19.team_g.project_rbsg.server.rest.army;
+
+import java.util.List;
+
+public class UnitType {
+
+    public String id;
+    public String type;
+    public Integer mp;
+    public Integer hp;
+    public List<String> canAttack;
+}
