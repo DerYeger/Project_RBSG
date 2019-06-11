ff89a28fd3d5cd9bc21bc4356a333ec7ead59194
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-11 17:10:29 +0200

    created UnitDetailController

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/UnitDetailController.java
@@ -0,0 +1,13 @@
+package de.uniks.se19.team_g.project_rbsg.army_builder;
+
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
+
+public class UnitDetailController {
+    public StackPane imageStackPane;
+    public ImageView imageView;
+    public TilePane statsContainer;
+    public TextField unitDescription;
+}
