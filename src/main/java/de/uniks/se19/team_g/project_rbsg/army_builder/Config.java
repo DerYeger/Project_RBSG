1f7fde26466fcaad652bc4c23c233410eebb45df
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-11 19:21:48 +0200

    added configuration to create UnitPropertyViewComponents

--- src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/Config.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/Config.java
@@ -1,17 +1,20 @@
 package de.uniks.se19.team_g.project_rbsg.army_builder;
 
 import de.uniks.se19.team_g.project_rbsg.ViewComponent;
+import de.uniks.se19.team_g.project_rbsg.army_builder.unit_detail.UnitPropertyController;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 
 /**
  * @author Goatfryed
  */
 @Configuration
+@Lazy
 public class Config {
 
     @Bean
@@ -19,6 +22,13 @@ public class Config {
     public ViewComponent<SceneController, Parent> armyBuilderScene(FXMLLoader fxmlLoader)
     {
         fxmlLoader.setLocation(getClass().getResource("/ui/army_builder/ArmyBuilderScene.fxml"));
+        return ViewComponent.fromLoader(fxmlLoader);
+    }
+
+    @Bean
+    @Scope("prototype")
+    public ViewComponent<UnitPropertyController, Parent> unitProperty(FXMLLoader fxmlLoader) {
+        fxmlLoader.setLocation(getClass().getResource("/ui/army_builder/UnitPropertyView.fxml"));
         return ViewComponent.fromLoader(fxmlLoader);
     }
 
