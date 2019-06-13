3eb827668142d23fd617bcae352a8793a62ea4eb
Kea Nu <kestue7@gmail.com>
2019-06-13 03:03:22 +0200

    fixed tests

--- src/test/java/de/uniks/se19/team_g/project_rbsg/army_builder/ArmyBuilderSceneTest.java
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/army_builder/ArmyBuilderSceneTest.java
@@ -1,5 +1,6 @@
 package de.uniks.se19.team_g.project_rbsg.army_builder;
 
+import de.uniks.se19.team_g.project_rbsg.SceneManager;
 import de.uniks.se19.team_g.project_rbsg.ViewComponent;
 import de.uniks.se19.team_g.project_rbsg.army_builder.unit_detail.UnitDetailController;
 import de.uniks.se19.team_g.project_rbsg.army_builder.unit_selection.UnitListEntryController;
@@ -37,7 +38,8 @@ import java.util.concurrent.CompletableFuture;
         UnitDetailController.class,
         UnitListEntryFactory.class,
         UnitListEntryController.class,
-        ArmyBuilderSceneTest.ContextConfiguration.class
+        ArmyBuilderSceneTest.ContextConfiguration.class,
+        SceneManager.class
 })
 public class ArmyBuilderSceneTest extends ApplicationTest {
 
@@ -75,7 +77,7 @@ public class ArmyBuilderSceneTest extends ApplicationTest {
 
         Assert.assertNotNull(controller.armyBuilderScene);
         Assert.assertNotNull(controller.content);
-        Assert.assertNotNull(controller.sideBarLeft);
+        Assert.assertNotNull(controller.leaveButton);
         Assert.assertNotNull(controller.sideBarRight);
         Assert.assertNotNull(controller.unitDetailView);
         Assert.assertNotNull(controller.armyView);
