e2b8bae980ee0cfd77705435d4aa594d68fbec11
Kea Nu <kestue7@gmail.com>
2019-06-13 03:24:02 +0200

    minor changes

--- src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/SceneController.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/SceneController.java
@@ -40,7 +40,6 @@ public class SceneController extends ButtonIconsSetter implements Initializable 
     private final ArmyBuilderState state;
     private final UnitListEntryFactory unitCellFactory;
     private final GetUnitTypesService getUnitTypesService;
-    public VBox sideBarLeft;
     public VBox content;
     public HBox topContentContainer;
     public ListView<Unit> unitListView;
@@ -108,7 +107,7 @@ public class SceneController extends ButtonIconsSetter implements Initializable 
 
     public void leaveRoom(ActionEvent actionEvent) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Leave Game");
+        alert.setTitle("Leave ArmyBuilder");
         alert.setHeaderText("Are you sure you want to exit?");
         alert.showAndWait();
         if (alert.getResult().equals(ButtonType.OK)) {
