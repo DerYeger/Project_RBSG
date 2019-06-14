0cc2c928f06901e9d4210ce02714f643d3088b29
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-14 18:18:25 +0200

    moved selected army from armybuilder state to application state

--- src/main/java/de/uniks/se19/team_g/project_rbsg/configuration/ApplicationState.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/configuration/ApplicationState.java
@@ -1,6 +1,7 @@
 package de.uniks.se19.team_g.project_rbsg.configuration;
 
 import de.uniks.se19.team_g.project_rbsg.model.Army;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import org.springframework.stereotype.Component;
@@ -8,5 +9,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class ApplicationState {
 
+    public final SimpleObjectProperty<Army> selectedArmy = new SimpleObjectProperty<>();
     public final ObservableList<Army> armies =  FXCollections.observableArrayList();
 }
