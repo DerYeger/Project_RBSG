926efd647b48935599f607b5e3fb4171c99e73d6
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-14 18:10:45 +0200

    extended models

--- src/main/java/de/uniks/se19/team_g/project_rbsg/model/Army.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/model/Army.java
@@ -1,9 +1,12 @@
 package de.uniks.se19.team_g.project_rbsg.model;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class Army {
 
+    public final SimpleStringProperty id = new SimpleStringProperty();
+    public final SimpleStringProperty name = new SimpleStringProperty();
     public final ObservableList<Unit> units = FXCollections.observableArrayList();
 }
