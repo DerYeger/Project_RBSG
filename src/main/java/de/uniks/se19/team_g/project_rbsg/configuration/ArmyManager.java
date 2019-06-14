628456e32419b42f1ea70a47c995e6557627ef7d
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-14 18:35:55 +0200

    autowired army manager

--- src/main/java/de/uniks/se19/team_g/project_rbsg/configuration/ArmyManager.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/configuration/ArmyManager.java
@@ -2,11 +2,13 @@ package de.uniks.se19.team_g.project_rbsg.configuration;
 
 import de.uniks.se19.team_g.project_rbsg.model.Army;
 import de.uniks.se19.team_g.project_rbsg.server.rest.army.GetArmiesService;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Nonnull;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
 
+@Component
 public class ArmyManager {
 
     @Nonnull private final GetArmiesService getArmiesService;
