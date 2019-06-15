6e733d21a48dff8e10b047191d2bbb1a4def96d7
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-15 15:28:42 +0200

    Added ArmySquadController

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/army/ArmySquadController.java
@@ -0,0 +1,11 @@
+package de.uniks.se19.team_g.project_rbsg.army_builder.army;
+
+import javafx.scene.control.Label;
+
+public class ArmySquadController {
+
+    public Label typeLabel;
+    public Label countLabel;
+
+
+}
