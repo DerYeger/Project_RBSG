db9effe4a0b2a8ffe28fd11b0128c684358c3113
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-16 09:35:06 +0200

    bound selected army to army detail controller, added component tags

--- src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/army/ArmyDetailController.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/army/ArmyDetailController.java
@@ -1,26 +1,50 @@
 package de.uniks.se19.team_g.project_rbsg.army_builder.army;
 
 import de.uniks.se19.team_g.project_rbsg.configuration.ApplicationState;
+import de.uniks.se19.team_g.project_rbsg.model.Army;
+import de.uniks.se19.team_g.project_rbsg.model.Unit;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.beans.value.WeakChangeListener;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import org.springframework.stereotype.Component;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+@Component
 public class ArmyDetailController implements Initializable {
+
     public Label armyNameLabel;
     public Label armySizeLabel;
-    public ListView armySquadList;
+
+    public ListView<Unit> armySquadList;
 
     private final ApplicationState state;
+    private ChangeListener<Army> selectedArmyListener;
 
     public ArmyDetailController(ApplicationState state) {
         this.state = state;
+        selectedArmyListener = this::onSelectedArmyUpdate;
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        state.selectedArmy.addListener(new WeakChangeListener<>(selectedArmyListener));
+        final Army selectedArmy = state.selectedArmy.get();
+        if (selectedArmy != null) {
+            bindToArmy(selectedArmy);
+        }
+    }
 
+    private void onSelectedArmyUpdate(ObservableValue<? extends Army> observableValue, Army prev, Army nextArmy) {
+        bindToArmy(nextArmy);
+    }
+
+    private void bindToArmy(Army army) {
+        armyNameLabel.textProperty().bind(army.name);
+        armySquadList.setItems(army.units);
     }
 }
--- src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/army/ArmySquadController.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/army/ArmySquadController.java
@@ -1,7 +1,9 @@
 package de.uniks.se19.team_g.project_rbsg.army_builder.army;
 
 import javafx.scene.control.Label;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ArmySquadController {
 
     public Label typeLabel;
