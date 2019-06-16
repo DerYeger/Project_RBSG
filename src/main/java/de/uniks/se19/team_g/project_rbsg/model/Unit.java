d9cd10ccf09cce12b1d9e62aea1d3c4586381c12
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-16 22:23:52 +0200

    fill army units with unit definition data on load

--- src/main/java/de/uniks/se19/team_g/project_rbsg/model/Unit.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/model/Unit.java
@@ -3,7 +3,11 @@ package de.uniks.se19.team_g.project_rbsg.model;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
-public class Unit {
+import javax.annotation.Nonnull;
+
+public class Unit implements Cloneable {
+
+    public static final String UNKNOWN = "UNKNOWN";
 
     public final SimpleStringProperty imageUrl = new SimpleStringProperty();
     public final SimpleStringProperty iconUrl = new SimpleStringProperty();
@@ -18,4 +22,29 @@ public class Unit {
     public final SimpleStringProperty name = new SimpleStringProperty();
     public final SimpleStringProperty description = new SimpleStringProperty();
     public final SimpleStringProperty id = new SimpleStringProperty();
+
+    public static Unit unknownType(String id) {
+        final Unit unit = new Unit();
+        unit.health.set(-1);
+        unit.physicalResistance.set(0);
+        unit.magicResistance.set(0);
+        unit.speed.set(0);
+        unit.attack.set(0);
+        unit.spellPower.set(0);
+        unit.name.set(UNKNOWN);
+        unit.description.set(UNKNOWN);
+        unit.id.set(id);
+
+        return unit;
+    }
+
+    @Override
+    @Nonnull
+    public Unit clone() {
+        try {
+            return (Unit) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
