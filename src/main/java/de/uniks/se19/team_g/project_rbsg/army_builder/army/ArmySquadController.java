13fd4a1cc5a4d12e2bd98431d1944e54053a342f
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-17 10:09:13 +0200

    updated army sqaud view to include image

--- src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/army/ArmySquadController.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/army/ArmySquadController.java
@@ -2,11 +2,13 @@ package de.uniks.se19.team_g.project_rbsg.army_builder.army;
 
 import de.uniks.se19.team_g.project_rbsg.army_builder.ArmyBuilderState;
 import de.uniks.se19.team_g.project_rbsg.model.Unit;
+import de.uniks.se19.team_g.project_rbsg.util.JavaFXUtils;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -19,7 +21,8 @@ public class ArmySquadController extends ListCell<Unit> implements Initializable
 
     public Label typeLabel;
     public Label countLabel;
-    public Parent root;
+    public Pane root;
+    public ImageView imageView;
 
     private Unit unit;
 
@@ -42,6 +45,7 @@ public class ArmySquadController extends ListCell<Unit> implements Initializable
 
         typeLabel.textProperty().bind(unit.name);
         countLabel.setText("1");
+        JavaFXUtils.bindImage(imageView.imageProperty(), unit.imageUrl);
 
         setGraphic(root);
     }
@@ -51,6 +55,8 @@ public class ArmySquadController extends ListCell<Unit> implements Initializable
     public void initialize(URL location, ResourceBundle resources) {
         setText(null);
         setGraphic(null);
+
+        root.prefHeightProperty().bindBidirectional(prefWidthProperty());
 
         root.setOnMouseClicked(this::onSelection);
     }
