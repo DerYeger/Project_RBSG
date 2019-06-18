4ca71131ecb5435437949d8d4b5d474b3010741a
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-18 15:53:56 +0200

    wired cell factory

--- src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/army_selection/ArmySelectorController.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/army_selection/ArmySelectorController.java
@@ -2,14 +2,29 @@ package de.uniks.se19.team_g.project_rbsg.army_builder.army_selection;
 
 import de.uniks.se19.team_g.project_rbsg.model.Army;
 import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 
-public class ArmySelectorController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ArmySelectorController implements Initializable {
 
     public ListView<Army> listView;
+
+    private final ArmySelectorCellFactory cellFactory;
+
+    public ArmySelectorController(ArmySelectorCellFactory cellFactory) {
+        this.cellFactory = cellFactory;
+    }
 
     public void setSelection(ObservableList<Army> armies)
     {
         listView.setItems(armies);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        listView.setCellFactory(cellFactory);
     }
 }
