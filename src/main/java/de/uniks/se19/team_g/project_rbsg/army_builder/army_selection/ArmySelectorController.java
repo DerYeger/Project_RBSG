f734ae357ef7d9ace1d20cda0670bd6fe5662a87
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-18 15:33:09 +0200

    added setter for armies to select

--- src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/army_selection/ArmySelectorController.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/army_builder/army_selection/ArmySelectorController.java
@@ -1,10 +1,15 @@
 package de.uniks.se19.team_g.project_rbsg.army_builder.army_selection;
 
+import de.uniks.se19.team_g.project_rbsg.model.Army;
+import javafx.collections.ObservableList;
 import javafx.scene.control.ListView;
 
 public class ArmySelectorController {
 
-    public ListView listView;
+    public ListView<Army> listView;
 
-
+    public void setSelection(ObservableList<Army> armies)
+    {
+        listView.setItems(armies);
+    }
 }
