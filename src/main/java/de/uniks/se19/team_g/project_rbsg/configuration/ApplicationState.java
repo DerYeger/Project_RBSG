936e9ceb691284ebc658fc22c79dcf45022bfe15
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-25 00:46:50 +0200

    introduced validArmySelected Flag in ApplicationState

--- src/main/java/de/uniks/se19/team_g/project_rbsg/configuration/ApplicationState.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/configuration/ApplicationState.java
@@ -2,7 +2,10 @@ package de.uniks.se19.team_g.project_rbsg.configuration;
 
 import de.uniks.se19.team_g.project_rbsg.model.Army;
 import de.uniks.se19.team_g.project_rbsg.model.Unit;
-import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.*;
+import javafx.beans.value.ObservableBooleanValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
@@ -14,15 +17,37 @@ public class ApplicationState {
     public final SimpleObjectProperty<Army> selectedArmy = new SimpleObjectProperty<>();
     public final ObservableList<Army> armies =  FXCollections.observableArrayList();
     public final ObservableList<Unit> unitDefinitions = FXCollections.observableArrayList();
+    public final SimpleBooleanProperty validArmySelected = new SimpleBooleanProperty();
 
-    public ApplicationState () {
+    public ApplicationState ()
+    {
+        validArmySelected.set(false);
+        selectedArmy.addListener((observable, oldValue, newValue) ->{
+            if (newValue == null) {
+                validArmySelected.unbind();
+                validArmySelected.setValue(false);
+            } else {
+                final Army army = selectedArmy.get();
+                final BooleanBinding validArmySizeBinding = new SimpleListProperty<>(army.units).sizeProperty().isEqualTo(Army.ARMY_MAX_SIZE);
+
+                validArmySelected.bind(
+                        army.id.isNotNull().and(validArmySizeBinding)
+                );
+            }
+        });
+
         armies.addListener(this::onArmyUpdate);
+
     }
 
     private void onArmyUpdate(ListChangeListener.Change<? extends Army> change) {
-        change.next();
-        if (selectedArmy.get() == null || change.getRemoved().contains(selectedArmy.get())) {
-            selectedArmy.set(change.getList().get(0));
+        while (change.next()) {
+            if (selectedArmy.get() == null || change.getRemoved().contains(selectedArmy.get())) {
+                selectedArmy.set(change.getList().get(0));
+                if (selectedArmy.get() != null) {
+                    break;
+                }
+            }
         }
     }
 }
