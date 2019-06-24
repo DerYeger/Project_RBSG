793cbb3abb9e93cdf9aa6f28781dcb7c63c10366
Jan Müller <janmueller3698@gmail.com>
2019-06-24 12:08:34 +0200

    Added preview map builder class

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/waiting_room/model/PreviewMapBuilder.java
@@ -0,0 +1,10 @@
+package de.uniks.se19.team_g.project_rbsg.waiting_room.model;
+
+import javafx.scene.Node;
+
+public class PreviewMapBuilder {
+
+    public Node buildPreviewMap() {
+        return null;
+    }
+}
