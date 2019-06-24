f39386be43742d65d73d0fdf122f2313856e2093
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-24 14:25:08 +0200

    added test

--- src/main/java/de/uniks/se19/team_g/project_rbsg/configuration/army/ArmyGeneratorStrategy.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/configuration/army/ArmyGeneratorStrategy.java
@@ -2,9 +2,11 @@ package de.uniks.se19.team_g.project_rbsg.configuration.army;
 
 import de.uniks.se19.team_g.project_rbsg.model.Army;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.List;
 
 public interface ArmyGeneratorStrategy {
 
-    Army createArmy(List<Army> armies);
+    @Nonnull Army createArmy(@Nullable List<Army> armies);
 }
