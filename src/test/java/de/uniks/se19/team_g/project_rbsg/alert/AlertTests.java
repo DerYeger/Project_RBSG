1f1448618d8267f075988b12a10e34618dbb426b
Jan Müller <janmueller3698@gmail.com>
2019-06-30 16:57:19 +0200

    Added tests

---
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/alert/AlertTests.java
@@ -0,0 +1,141 @@
+package de.uniks.se19.team_g.project_rbsg.alert;
+
+import de.uniks.se19.team_g.project_rbsg.SceneManager;
+import de.uniks.se19.team_g.project_rbsg.configuration.AlertConfig;
+import de.uniks.se19.team_g.project_rbsg.configuration.SceneManagerConfig;
+import io.rincl.Rincl;
+import io.rincl.resourcebundle.ResourceBundleResourceI18nConcern;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.lang.NonNull;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.util.WaitForAsyncUtils;
+
+import java.util.Locale;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {
+        AlertTests.ContextConfiguration.class,
+        SceneManagerConfig.class,
+        AlertConfig.class,
+        SceneManager.class,
+        AlertBuilder.class,
+        InfoAlertController.class,
+        ConfirmationAlertController.class
+})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class AlertTests extends ApplicationTest implements ApplicationContextAware {
+
+    private ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(@NonNull final ApplicationContext context) throws BeansException {
+        this.context = context;
+    }
+
+    @TestConfiguration
+    static class ContextConfiguration implements ApplicationContextAware {
+
+        private ApplicationContext context;
+
+        @Override
+        public void setApplicationContext(@NonNull final ApplicationContext context) throws BeansException {
+            this.context = context;
+        }
+
+        @Bean
+        @Scope("prototype")
+        public FXMLLoader fxmlLoader()
+        {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setControllerFactory(this.context::getBean);
+            return fxmlLoader;
+        }
+    }
+
+    private SceneManager sceneManager;
+
+    private Scene scene;
+
+    @Override
+    public void start(@NonNull final Stage stage) {
+        Rincl.setDefaultResourceI18nConcern(new ResourceBundleResourceI18nConcern());
+        Rincl.setLocale(Locale.ENGLISH);
+        sceneManager = context.getBean(SceneManager.class);
+        sceneManager.init(stage);
+        scene = new Scene(new StackPane(), 400, 400);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Test
+    public void testInfoAlert() {
+        final AlertBuilder alertBuilder = context.getBean(AlertBuilder.class);
+        alertBuilder.information(AlertBuilder.Text.INVALID_INPUT);
+        WaitForAsyncUtils.waitForFxEvents();
+
+        final Button confirm = lookup("#confirm").queryButton();
+        assertNotNull(confirm);
+        assertNotNull(lookup("Invalid input"));
+
+        clickOn(confirm);
+        WaitForAsyncUtils.waitForFxEvents();
+
+        assertTrue(sceneManager.getAlertTarget().getChildren().isEmpty());
+    }
+
+    @Test
+    public void testConfirmationAlert() {
+        final boolean[] confirmedAndCanceled = new boolean[2];
+        final AlertBuilder alertBuilder = context.getBean(AlertBuilder.class);
+        alertBuilder.confirmation(AlertBuilder.Text.INVALID_INPUT,
+                        () -> confirmedAndCanceled[0] = true,
+                        () -> confirmedAndCanceled[1] = true);
+        WaitForAsyncUtils.waitForFxEvents();
+
+        final Button confirm = lookup("#confirm").queryButton();
+        assertNotNull(confirm);
+        assertNotNull(lookup("Invalid input"));
+
+        clickOn(confirm);
+        WaitForAsyncUtils.waitForFxEvents();
+
+        assertTrue(confirmedAndCanceled[0]);
+
+        final Button cancel = lookup("#cancel").queryButton();
+        assertNotNull(cancel);
+
+        WaitForAsyncUtils.waitForFxEvents();
+        clickOn(cancel);
+        WaitForAsyncUtils.waitForFxEvents();
+
+        assertTrue(sceneManager.getAlertTarget().getChildren().isEmpty());
+        assertTrue(confirmedAndCanceled[1]);
+    }
+
+    @Test
+    public void testException() {
+        scene.setRoot(new Pane());
+        final AlertBuilder alertBuilder = context.getBean(AlertBuilder.class);
+        alertBuilder.information(AlertBuilder.Text.INVALID_INPUT);
+        WaitForAsyncUtils.waitForFxEvents();
+        assertTrue(scene.getRoot().getChildrenUnmodifiable().isEmpty());
+    }
+}
