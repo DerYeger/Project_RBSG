641486ea563f0a72659dc8c2fbb3e4dc5e257a05
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-07-02 20:23:29 +0200

    added hasUnsavedUpdates flag to army model

--- src/main/java/de/uniks/se19/team_g/project_rbsg/model/Army.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/model/Army.java
@@ -14,6 +14,8 @@ public class Army {
     public final SimpleListProperty<Unit> simpleUnits = new SimpleListProperty<>(units);
     public final ReadOnlyBooleanProperty isPlayable;
 
+    private final BooleanProperty hasUnsavedUpdates = new SimpleBooleanProperty();
+
     public final SimpleObjectProperty<ArmyIcon> iconType = new SimpleObjectProperty<>(ArmyIcon.DRAGON_HEAD);
 
 
@@ -21,5 +23,17 @@ public class Army {
         SimpleBooleanProperty isPlayable = new SimpleBooleanProperty();
         isPlayable.bind(id.isNotNull().and(simpleUnits.sizeProperty().isEqualTo(ApplicationState.ARMY_MAX_UNIT_COUNT)));
         this.isPlayable = isPlayable;
+    }
+
+    public ReadOnlyBooleanProperty hasUnsavedUpdatesProperty() {
+        return hasUnsavedUpdates;
+    }
+
+    public boolean hasUnsavedUpdates() {
+        return hasUnsavedUpdates.get();
+    }
+
+    public void setHasUnsavedUpdates(boolean hasUnsavedUpdates) {
+        this.hasUnsavedUpdates.set(hasUnsavedUpdates);
     }
 }
