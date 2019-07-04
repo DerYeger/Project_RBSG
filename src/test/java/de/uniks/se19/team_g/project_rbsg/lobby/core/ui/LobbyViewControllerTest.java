c8bd9bda9f844e926dae6e0d74e45eab81566bf1
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-07-04 16:31:24 +0200

    fixed tests by mocking away dependencies, so we don't need to follow the rabbit hole any further

--- src/test/java/de/uniks/se19/team_g/project_rbsg/lobby/core/ui/LobbyViewControllerTest.java
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/lobby/core/ui/LobbyViewControllerTest.java
@@ -4,16 +4,13 @@ import de.uniks.se19.team_g.project_rbsg.MusicManager;
 import de.uniks.se19.team_g.project_rbsg.SceneManager;
 import de.uniks.se19.team_g.project_rbsg.alert.AlertBuilder;
 import de.uniks.se19.team_g.project_rbsg.chat.ChatController;
-import de.uniks.se19.team_g.project_rbsg.configuration.ApplicationState;
 import de.uniks.se19.team_g.project_rbsg.lobby.chat.LobbyChatClient;
 import de.uniks.se19.team_g.project_rbsg.lobby.core.PlayerManager;
 import de.uniks.se19.team_g.project_rbsg.lobby.game.CreateGameFormBuilder;
 import de.uniks.se19.team_g.project_rbsg.lobby.game.GameManager;
 import de.uniks.se19.team_g.project_rbsg.lobby.system.SystemMessageManager;
-import de.uniks.se19.team_g.project_rbsg.model.GameProvider;
 import de.uniks.se19.team_g.project_rbsg.model.UserProvider;
 import de.uniks.se19.team_g.project_rbsg.server.rest.DefaultLogoutManager;
-import de.uniks.se19.team_g.project_rbsg.server.rest.JoinGameManager;
 import org.junit.Test;
 import org.springframework.beans.factory.ObjectFactory;
 
@@ -27,10 +24,8 @@ public class LobbyViewControllerTest {
         final SceneManager sceneManager = mock(SceneManager.class);
         @SuppressWarnings("unchecked") final ObjectFactory<GameListViewCell> mock = mock(ObjectFactory.class);
         LobbyViewController sut = new LobbyViewController(
-                mock(GameProvider.class),
                 mock(UserProvider.class),
                 sceneManager,
-                mock(JoinGameManager.class),
                 mock(PlayerManager.class),
                 mock(GameManager.class),
                 mock(SystemMessageManager.class),
