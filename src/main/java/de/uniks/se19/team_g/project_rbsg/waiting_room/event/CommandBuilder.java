d0019b17be0f6e7832b9f5904fb45aeada9d803d
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-07-09 16:58:28 +0200

    ready and start game command added to command builder

--- src/main/java/de/uniks/se19/team_g/project_rbsg/waiting_room/event/CommandBuilder.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/waiting_room/event/CommandBuilder.java
@@ -3,6 +3,9 @@ package de.uniks.se19.team_g.project_rbsg.waiting_room.event;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CommandBuilder {
 
     private static String MESSAGE_TYPE = "messageType";
@@ -18,5 +21,19 @@ public class CommandBuilder {
                 .createObjectNode()
                 .put(MESSAGE_TYPE, COMMAND)
                 .put(ACTION, LEAVE_GAME);
+    }
+
+    public static Map<String,Object> readyToPlay() {
+        Map<String, Object> command = new HashMap<>();
+        command.put(MESSAGE_TYPE, COMMAND);
+        command.put(ACTION, "readyToPlay");
+        return command;
+    }
+
+    public static Map<String,Object> startGame() {
+        Map<String, Object> command = new HashMap<>();
+        command.put(MESSAGE_TYPE, COMMAND);
+        command.put(ACTION, "startGame");
+        return command;
     }
 }
