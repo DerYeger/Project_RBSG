96957f97675a88d5986a36b837eab547fe2c4c65
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-07-11 20:41:43 +0200

    Ingame context created to provide access to ingame relevant stuff

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/IngameContext.java
@@ -0,0 +1,66 @@
+package de.uniks.se19.team_g.project_rbsg.ingame;
+
+import de.uniks.se19.team_g.project_rbsg.ingame.waiting_room.event.GameEventManager;
+import de.uniks.se19.team_g.project_rbsg.model.*;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ReadOnlyBooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Nonnull;
+
+@Component
+@Scope("prototype")
+public class IngameContext {
+
+    @Nonnull private final UserProvider userProvider;
+    @Nonnull private final GameProvider gameDataProvider;
+    @Nonnull private final IngameGameProvider gameStateProvider;
+    private GameEventManager gameEventManager;
+
+    private final BooleanProperty initialized = new SimpleBooleanProperty();
+
+    public IngameContext(
+        @Nonnull UserProvider userProvider,
+        @Nonnull GameProvider gameDataProvider,
+        @Nonnull IngameGameProvider gameStateProvider
+    ) {
+        this.userProvider = userProvider;
+        this.gameDataProvider = gameDataProvider;
+        this.gameStateProvider = gameStateProvider;
+    }
+
+
+    public boolean isInitialized() {
+        return initialized.get();
+    }
+
+    public ReadOnlyBooleanProperty initializedProperty() {
+        return initialized;
+    }
+
+    public void gameInitialized() {
+        initialized.set(true);
+    }
+
+    public User getUser() {
+        return userProvider.get();
+    }
+
+    public de.uniks.se19.team_g.project_rbsg.model.Game getGameData() {
+        return gameDataProvider.get();
+    }
+
+    public de.uniks.se19.team_g.project_rbsg.ingame.waiting_room.model.Game getGameState() {
+        return gameStateProvider.get();
+    }
+
+    public GameEventManager getGameEventManager() {
+        return gameEventManager;
+    }
+
+    public void setGameEventManager(GameEventManager gameEventManager) {
+        this.gameEventManager = gameEventManager;
+    }
+}
--- src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/IngameRootController.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/IngameRootController.java
@@ -9,38 +9,49 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.layout.StackPane;
 import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Nonnull;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ * TODO: consider removing scope prototype and instead introduce something like Bootable Interface for boot/terminate/boot/...
+ */
 @Component
+@Scope("prototype")
 public class IngameRootController
     implements RootController, Initializable, Terminable
 {
 
     public StackPane root;
 
-    @Nonnull
-    private final ObjectFactory<ViewComponent<WaitingRoomViewController>> waitingRoomFactory;
+    @Nonnull private final ObjectFactory<ViewComponent<WaitingRoomViewController>> waitingRoomFactory;
+    @Nonnull private final ObjectFactory<ViewComponent<IngameViewController>> ingameFactory;
+    @Nonnull private final ObjectFactory<IngameContext> contextFactory;
 
-    @Nonnull
-    private final ObjectFactory<ViewComponent<IngameViewController>> ingameFactory;
+    private IngameContext ingameContext;
+
 
     private ViewComponent activeComponent;
 
     public IngameRootController(
         @Nonnull ObjectFactory<ViewComponent<WaitingRoomViewController>> waitingRoomFactory,
-        @Nonnull ObjectFactory<ViewComponent<IngameViewController>> ingameFactory
+        @Nonnull ObjectFactory<ViewComponent<IngameViewController>> ingameFactory,
+        @Nonnull ObjectFactory<IngameContext> contextFactory
     ) {
         this.waitingRoomFactory = waitingRoomFactory;
         this.ingameFactory = ingameFactory;
+        this.contextFactory = contextFactory;
     }
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        ingameContext = contextFactory.getObject();
+
         activeComponent = waitingRoomFactory.getObject();
         final Node root = activeComponent.getRoot();
         this.root.getChildren().add(root);
