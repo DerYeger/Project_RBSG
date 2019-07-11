267d054a7dc9bc9e10786be7f3d95db3389fe2f0
millenium9977 <siebertgeorg@t-online.de>
2019-07-12 01:27:46 +0200

    Enhanced TileHighlighting

--- src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/uiModel/TileHighlighting.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/uiModel/TileHighlighting.java
@@ -1,6 +1,21 @@
 package de.uniks.se19.team_g.project_rbsg.ingame.uiModel;
 
+import org.springframework.lang.NonNull;
+
 public enum TileHighlighting
 {
-    NONE, HOVERED, MOVE, ATTACK;
+    NONE("None"), HOVERED("Hovered"), SELECTED("Selected"), MOVE("Move"), ATTACK("Attack");
+
+    private final String highlight;
+
+    TileHighlighting(@NonNull String highlight)
+    {
+        this.highlight = highlight;
+    }
+
+    @Override
+    public String toString() {
+        return highlight;
+    }
+
 }
