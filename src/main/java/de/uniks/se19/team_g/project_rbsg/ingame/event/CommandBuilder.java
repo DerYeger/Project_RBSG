5b1e24039936aa7d92353e0f066431240c88798f
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-07-17 16:10:30 +0200

    build moveUnit command

--- src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/event/CommandBuilder.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/event/CommandBuilder.java
@@ -2,10 +2,14 @@ package de.uniks.se19.team_g.project_rbsg.ingame.event;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import de.uniks.se19.team_g.project_rbsg.ingame.model.Cell;
+import de.uniks.se19.team_g.project_rbsg.ingame.model.Unit;
 import de.uniks.se19.team_g.project_rbsg.model.Army;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CommandBuilder {
 
@@ -50,5 +54,22 @@ public class CommandBuilder {
         command.put(ACTION, action);
         return command;
 
+    }
+
+    public static Map<String, Object> moveUnit(Unit unit, List<Cell> sortedList)
+    {
+        MoveUnitData data = new MoveUnitData();
+        data.unitId = unit.getId();
+        data.path = sortedList.stream().map(Cell::getId).collect(Collectors.toList());
+
+        Map<String, Object> command = command("moveUnit");
+        command.put("data", data);
+
+        return command;
+    }
+
+    static class MoveUnitData {
+        public String unitId;
+        public List<String> path;
     }
 }
