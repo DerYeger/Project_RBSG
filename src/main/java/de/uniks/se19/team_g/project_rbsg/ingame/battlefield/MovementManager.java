89e19332fb82031fb09142afeb8c9b7d1e86003e
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-07-17 14:11:56 +0200

    introduced movement manager stub

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/battlefield/MovementManager.java
@@ -0,0 +1,22 @@
+package de.uniks.se19.team_g.project_rbsg.ingame.battlefield;
+
+import de.uniks.se19.team_g.project_rbsg.ingame.model.Cell;
+import de.uniks.se19.team_g.project_rbsg.ingame.model.Unit;
+
+import javax.annotation.Nullable;
+
+public class MovementManager {
+
+    private final MovementEvaluator movementEvaluator;
+
+    public MovementManager(MovementEvaluator movementEvaluator) {
+
+        this.movementEvaluator = movementEvaluator;
+    }
+
+    @Nullable
+    public Tour getTour(Unit unit, Cell target1) {
+
+        return null;
+    }
+}
