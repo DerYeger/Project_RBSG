c2b87095bcdd617100419c496d58e7cf4fdbb6d1
Juri Lozowoj <juri.lozowoj@freenet.de>
2019-07-17 15:36:58 +0200

    Cherrypicked MovementManager

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/battlefield/MovementManager.java
@@ -0,0 +1,15 @@
+package de.uniks.se19.team_g.project_rbsg.ingame.battlefield;
+
+import de.uniks.se19.team_g.project_rbsg.ingame.model.Cell;
+import de.uniks.se19.team_g.project_rbsg.ingame.model.Unit;
+
+import javax.annotation.Nullable;
+
+public class MovementManager {
+
+    @Nullable
+    public Tour getTour(Unit unit, Cell target1) {
+
+        return null;
+    }
+}
