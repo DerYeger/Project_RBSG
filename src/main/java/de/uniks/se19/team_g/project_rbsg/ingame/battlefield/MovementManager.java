5e2ae39faec46f7ccedad41c294c19dfee741ab1
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-07-17 14:38:32 +0200

    motivations

--- src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/battlefield/MovementManager.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/battlefield/MovementManager.java
@@ -5,6 +5,9 @@ import de.uniks.se19.team_g.project_rbsg.ingame.model.Unit;
 
 import javax.annotation.Nullable;
 
+/**
+ * The movement manager should provide information about cells that can be reached for a given unit
+ */
 public class MovementManager {
 
     private final MovementEvaluator movementEvaluator;
