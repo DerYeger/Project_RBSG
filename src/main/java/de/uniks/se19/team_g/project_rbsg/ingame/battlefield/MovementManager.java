fc6ab1075e6f4af9d9497169cbb6dcc09eabc17c
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-07-17 15:04:57 +0200

    implemented movement manager to manage movements for a single unit

--- src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/battlefield/MovementManager.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/battlefield/MovementManager.java
@@ -2,8 +2,12 @@ package de.uniks.se19.team_g.project_rbsg.ingame.battlefield;
 
 import de.uniks.se19.team_g.project_rbsg.ingame.model.Cell;
 import de.uniks.se19.team_g.project_rbsg.ingame.model.Unit;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.WeakChangeListener;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Map;
 
 /**
  * The movement manager should provide information about cells that can be reached for a given unit
@@ -12,14 +16,32 @@ public class MovementManager {
 
     private final MovementEvaluator movementEvaluator;
 
+    private Map<Cell, Tour> allowedTours;
+    private Unit unit;
+    private ChangeListener<Cell> unitPositionListener;
+
     public MovementManager(MovementEvaluator movementEvaluator) {
 
         this.movementEvaluator = movementEvaluator;
     }
 
     @Nullable
-    public Tour getTour(Unit unit, Cell target1) {
+    public Tour getTour(final @Nonnull Unit unit, final @Nonnull Cell target1) {
+        if (this.unit != unit) {
+            manageUnit(unit);
+        }
 
-        return null;
+        return allowedTours.get(target1);
+    }
+
+    protected void manageUnit(final @Nonnull Unit unit) {
+        this.unit = unit;
+        allowedTours = movementEvaluator.getAllowedTours(unit);
+
+        unitPositionListener = (observable, oldValue, newValue) -> {
+            this.unit = null;
+            this.allowedTours = null;
+        };
+        this.unit.positionProperty().addListener(new WeakChangeListener<>(unitPositionListener));
     }
 }
