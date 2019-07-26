17efc2375ff7e48f909013e8709d18e00d36d6e9
vakanto <vakanto@posteo.eu>
2019-07-26 11:33:02 +0200

    "Fixed" testAttackTest

--- src/test/java/de/uniks/se19/team_g/project_rbsg/ingame/battlefield/BattleFieldViewTest.java
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/ingame/battlefield/BattleFieldViewTest.java
@@ -478,8 +478,9 @@ public class BattleFieldViewTest extends ApplicationTest {
         verifyZeroInteractions(gameEventManager);
         game.setPhase(Game.Phase.attackPhase.name());
 
-        click(300, 200);
-        click(350, 200);
+        click(300, 225);
+        click(300, 225);
+        click(350, 225);
         verify(gameEventManager).api();
         verifyNoMoreInteractions(gameEventManager);
         verify(ingameApi).attack(definition.playerUnit, definition.otherUnit);
