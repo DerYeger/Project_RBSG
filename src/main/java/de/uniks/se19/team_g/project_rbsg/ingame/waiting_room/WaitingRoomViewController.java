29d7474a3cebe24554c42c723f13889c4c70c318
Juri Lozowoj <juri.lozowoj@freenet.de>
2019-07-31 10:20:52 +0200

    Enabled Chat for specatating users

--- src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/waiting_room/WaitingRoomViewController.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/waiting_room/WaitingRoomViewController.java
@@ -158,9 +158,12 @@ public class WaitingRoomViewController implements RootController, IngameViewCont
         final ViewComponent<ChatController> chatComponents = chatBuilder.buildChat(context.getGameEventManager());
         chatContainer.getChildren().add(chatComponents.getRoot());
         chatController = chatComponents.getController();
+        /* Chat is enbaled for user in spectator modus
+            by commenting in the following lines the chat be disabled again
         if(this.context.getGameData().isSpectatorModus()){
             chatController.getChatChannelControllers().get("General").getInputField().setDisable(true);
         }
+         */
     }
 
     private void initPlayerCardBuilders() {
