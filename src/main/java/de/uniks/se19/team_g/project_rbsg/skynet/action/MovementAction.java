b4e7e79985e97e3d28d394fc6c0a2f1e66ac1051
Jan Müller <janmueller3698@gmail.com>
2019-08-05 19:40:31 +0200

    Created interface implementations

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/skynet/action/MovementAction.java
@@ -0,0 +1,11 @@
+package de.uniks.se19.team_g.project_rbsg.skynet.action;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class MovementAction implements Action {
+
+    @Override
+    public ObjectNode getServerCommand() {
+        return null;
+    }
+}
---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/skynet/behaviour/MovementBehaviour.java
@@ -0,0 +1,14 @@
+package de.uniks.se19.team_g.project_rbsg.skynet.behaviour;
+
+import de.uniks.se19.team_g.project_rbsg.ingame.model.Game;
+import de.uniks.se19.team_g.project_rbsg.ingame.model.Player;
+import de.uniks.se19.team_g.project_rbsg.skynet.action.Action;
+import org.springframework.lang.NonNull;
+
+public class MovementBehaviour implements Behaviour {
+
+    @Override
+    public Action apply(@NonNull final Game game, @NonNull final Player player) {
+        return null;
+    }
+}
