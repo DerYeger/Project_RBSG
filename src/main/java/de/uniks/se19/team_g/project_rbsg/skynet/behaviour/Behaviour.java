40dbfe301dc58e5523c9fa96478205e386ae758c
Jan Müller <janmueller3698@gmail.com>
2019-08-05 19:42:36 +0200

    Changed return type to optional

--- src/main/java/de/uniks/se19/team_g/project_rbsg/skynet/behaviour/Behaviour.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/skynet/behaviour/Behaviour.java
@@ -5,8 +5,10 @@ import de.uniks.se19.team_g.project_rbsg.ingame.model.Player;
 import de.uniks.se19.team_g.project_rbsg.skynet.action.Action;
 import org.springframework.lang.NonNull;
 
+import java.util.Optional;
+
 @FunctionalInterface
 public interface Behaviour {
 
-    Action apply(@NonNull final Game game, @NonNull final Player player);
+    Optional<Action> apply(@NonNull final Game game, @NonNull final Player player);
 }
--- src/main/java/de/uniks/se19/team_g/project_rbsg/skynet/behaviour/MovementBehaviour.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/skynet/behaviour/MovementBehaviour.java
@@ -5,10 +5,12 @@ import de.uniks.se19.team_g.project_rbsg.ingame.model.Player;
 import de.uniks.se19.team_g.project_rbsg.skynet.action.Action;
 import org.springframework.lang.NonNull;
 
+import java.util.Optional;
+
 public class MovementBehaviour implements Behaviour {
 
     @Override
-    public Action apply(@NonNull final Game game, @NonNull final Player player) {
+    public Optional<Action> apply(@NonNull final Game game, @NonNull final Player player) {
         return null;
     }
 }
