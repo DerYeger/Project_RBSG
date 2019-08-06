18be5e9c67b8448970415ba527e40c314adb54ab
Jan Müller <janmueller3698@gmail.com>
2019-08-06 16:45:48 +0200

    Created AttackBehaviour

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/skynet/behaviour/AttackBehaviour.java
@@ -0,0 +1,16 @@
+package de.uniks.se19.team_g.project_rbsg.skynet.behaviour;
+
+import de.uniks.se19.team_g.project_rbsg.ingame.model.Game;
+import de.uniks.se19.team_g.project_rbsg.ingame.model.Player;
+import de.uniks.se19.team_g.project_rbsg.skynet.action.Action;
+import org.springframework.lang.NonNull;
+
+import java.util.Optional;
+
+public class AttackBehaviour implements Behaviour {
+    
+    @Override
+    public Optional<Action> apply(@NonNull final Game game, @NonNull final Player player) {
+        return Optional.empty();
+    }
+}
