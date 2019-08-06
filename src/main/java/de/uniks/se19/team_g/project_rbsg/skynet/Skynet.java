c3b91a79d8a0d04c1c488e18df9a72e0ff2d42a9
Jan Müller <janmueller3698@gmail.com>
2019-08-06 17:38:49 +0200

    Changed Behaviour selection

--- src/main/java/de/uniks/se19/team_g/project_rbsg/skynet/Skynet.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/skynet/Skynet.java
@@ -4,10 +4,12 @@ import de.uniks.se19.team_g.project_rbsg.ingame.model.Game;
 import de.uniks.se19.team_g.project_rbsg.ingame.model.Player;
 import de.uniks.se19.team_g.project_rbsg.skynet.action.Action;
 import de.uniks.se19.team_g.project_rbsg.skynet.action.ActionExecutor;
+import de.uniks.se19.team_g.project_rbsg.skynet.action.AttackAction;
 import de.uniks.se19.team_g.project_rbsg.skynet.action.MovementAction;
 import de.uniks.se19.team_g.project_rbsg.skynet.behaviour.Behaviour;
 import org.springframework.lang.NonNull;
 
+import java.util.HashMap;
 import java.util.Optional;
 
 public class Skynet {
@@ -16,8 +18,7 @@ public class Skynet {
     private final Game game;
     private final Player player;
 
-    //PLACEHOLDER
-    private Behaviour currentBehaviour;
+    private HashMap<String, Behaviour> behaviours;
 
     public Skynet(@NonNull final ActionExecutor actionExecutor,
                   @NonNull final Game game,
@@ -25,26 +26,41 @@ public class Skynet {
         this.actionExecutor = actionExecutor;
         this.game = game;
         this.player = player;
+
+        behaviours = new HashMap<>();
     }
 
-    public Skynet setCurrentBehaviour(@NonNull final Behaviour behaviour) {
-        currentBehaviour = behaviour;
+    public Skynet addBehaviour(@NonNull final Behaviour behaviour,
+                               @NonNull final String ...keys) {
+        for (final String key : keys) {
+            behaviours.put(key, behaviour);
+        }
         return this;
     }
 
     public Skynet turn() {
-        if (currentBehaviour == null || !game.getCurrentPlayer().equals(player) || game.getPhase().equals("attackPhase")) {
+        if (!game.getCurrentPlayer().equals(player)) {
             //ignore for now
             return this;
         }
 
+        final Behaviour currentBehaviour = getCurrentBehaviour();
+        if (currentBehaviour == null) return this;
+
         final Optional<Action> action = currentBehaviour.apply(game, player);
+
         if (action.isEmpty()) {
             //ignore for now
         } else if (action.get() instanceof MovementAction) {
             actionExecutor.execute((MovementAction) action.get());
+        } else if (action.get() instanceof AttackAction) {
+            actionExecutor.execute((AttackAction) action.get());
         }
 
         return this;
+    }
+
+    private Behaviour getCurrentBehaviour() {
+        return behaviours.get(game.getPhase());
     }
 }
