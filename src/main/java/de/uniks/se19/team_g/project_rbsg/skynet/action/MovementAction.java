190090f75598e7b5ec2a6a3f42e1f910ca32d58a
Jan Müller <janmueller3698@gmail.com>
2019-08-06 16:44:45 +0200

    Created AttackAction

--- src/main/java/de/uniks/se19/team_g/project_rbsg/skynet/action/MovementAction.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/skynet/action/MovementAction.java
@@ -1,11 +1,8 @@
 package de.uniks.se19.team_g.project_rbsg.skynet.action;
 
 import de.uniks.se19.team_g.project_rbsg.ingame.battlefield.Tour;
-import de.uniks.se19.team_g.project_rbsg.ingame.event.CommandBuilder;
 import de.uniks.se19.team_g.project_rbsg.ingame.model.Unit;
 import org.springframework.lang.NonNull;
-
-import java.util.Map;
 
 public class MovementAction implements Action {
 
@@ -16,10 +13,5 @@ public class MovementAction implements Action {
                           @NonNull final Tour tour) {
         this.unit = unit;
         this.tour = tour;
-    }
-
-    @Override
-    public Map<String, Object> getServerCommand() {
-        return CommandBuilder.moveUnit(unit, tour.getPath());
     }
 }
