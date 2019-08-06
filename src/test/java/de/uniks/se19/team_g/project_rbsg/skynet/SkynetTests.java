6a4df6dc7a3a8708ebc4c8c413f6931c50f36ae1
Jan Müller <janmueller3698@gmail.com>
2019-08-06 17:39:17 +0200

    Added additional Skynet tests

--- src/test/java/de/uniks/se19/team_g/project_rbsg/skynet/SkynetTests.java
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/skynet/SkynetTests.java
@@ -7,7 +7,9 @@ import de.uniks.se19.team_g.project_rbsg.ingame.model.Game;
 import de.uniks.se19.team_g.project_rbsg.ingame.model.Player;
 import de.uniks.se19.team_g.project_rbsg.ingame.model.Unit;
 import de.uniks.se19.team_g.project_rbsg.skynet.action.ActionExecutor;
+import de.uniks.se19.team_g.project_rbsg.skynet.action.AttackAction;
 import de.uniks.se19.team_g.project_rbsg.skynet.action.MovementAction;
+import de.uniks.se19.team_g.project_rbsg.skynet.behaviour.AttackBehaviour;
 import de.uniks.se19.team_g.project_rbsg.skynet.behaviour.MovementBehaviour;
 import org.junit.Test;
 
@@ -18,7 +20,7 @@ import static org.mockito.Mockito.*;
 public class SkynetTests {
 
     @Test
-    public void testSkynetTurn() {
+    public void testSkynetMoveTurn() {
         final Player player = new Player("skynet");
         final Unit unit = new Unit("testUnit")
                 .setRemainingMovePoints(5);
@@ -31,7 +33,7 @@ public class SkynetTests {
         final ActionExecutor actionExecutor = new ActionExecutor(api);
         final MovementBehaviour movementBehaviour = mock(MovementBehaviour.class);
         final Skynet skynet = new Skynet(actionExecutor, game, player)
-                .setCurrentBehaviour(movementBehaviour);
+                .addBehaviour(movementBehaviour, "movePhase");
 
         final MovementAction action = new MovementAction(unit, new Tour());
 
@@ -41,6 +43,32 @@ public class SkynetTests {
         skynet.turn();
 
         verify(api).move(action.unit, action.tour);
+    }
+
+    @Test
+    public void testSkynetAttackTurn() {
+        final TestGameBuilder.Definition definition = TestGameBuilder.sampleGameAttack();
+
+        final Unit unit = definition.playerUnit;
+        final Player player = unit.getLeader();
+        final Game game = definition.game
+                .setCurrentPlayer(player)
+                .setPhase("attackPhase");
+
+        final IngameApi api = mock(IngameApi.class);
+        final ActionExecutor actionExecutor = new ActionExecutor(api);
+        final AttackBehaviour attackBehaviour = mock(AttackBehaviour.class);
+        final Skynet skynet = new Skynet(actionExecutor, game, player)
+                .addBehaviour(attackBehaviour, "attackPhase");
+
+        final AttackAction action = new AttackAction(unit, definition.otherUnit);
+
+        when(attackBehaviour.apply(game, player))
+                .thenReturn(Optional.of(action));
+
+        skynet.turn();
+
+        verify(api).attack(action.unit, action.target);
     }
 
     @Test
@@ -57,7 +85,7 @@ public class SkynetTests {
         final ActionExecutor actionExecutor = new ActionExecutor(api);
         final MovementBehaviour movementBehaviour = mock(MovementBehaviour.class);
         final Skynet skynet = new Skynet(actionExecutor, game, player)
-                .setCurrentBehaviour(movementBehaviour);
+                .addBehaviour(movementBehaviour, "movePhase");
 
         final MovementAction action = new MovementAction(unit, new Tour());
 
