6e05086aad5b02917f788a7f610033ffae3d4707
millenium9977 <siebertgeorg@t-online.de>
2019-08-09 19:43:00 +0200

    Test is now an ApplicationTest

--- src/test/java/de/uniks/se19/team_g/project_rbsg/skynet/BotTest.java
+++ src/test/java/de/uniks/se19/team_g/project_rbsg/skynet/BotTest.java
@@ -5,14 +5,15 @@ import de.uniks.se19.team_g.project_rbsg.ingame.model.Game;
 import de.uniks.se19.team_g.project_rbsg.ingame.model.Player;
 import de.uniks.se19.team_g.project_rbsg.skynet.action.ActionExecutor;
 import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
-public class BotTest
+public class BotTest extends ApplicationTest
 {
     @Test
-    public void testBot() throws InterruptedException
+    public void testBot()  throws InterruptedException
     {
         Skynet skynet = new Skynet(new ActionExecutor(new IngameApi()), new Game("2"), new Player("2"))
         {
