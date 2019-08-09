dc35ff22161a77588b32140f60c3b95cd5c9197f
millenium9977 <siebertgeorg@t-online.de>
2019-08-09 12:46:10 +0200

    Added bug report button and opens email on windows

--- src/main/java/de/uniks/se19/team_g/project_rbsg/lobby/core/ui/LobbyViewController.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/lobby/core/ui/LobbyViewController.java
@@ -43,7 +43,10 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.awt.*;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
@@ -83,13 +86,7 @@ public class LobbyViewController implements RootController, Terminable, Rincled
 
     public VBox modal;
     public Pane modalBackground;
-
-    private ChatBuilder chatBuilder;
-    private ChatController chatController;
-    private CreateGameFormBuilder createGameFormBuilder;
-
-    private Node gameForm;
-
+    public Button bugReportButton;
     public StackPane mainStackPane;
     public Button soundButton;
     public Button logoutButton;
@@ -103,6 +100,10 @@ public class LobbyViewController implements RootController, Terminable, Rincled
     public ListView<Player> lobbyPlayerListView;
     public ListView<Game> lobbyGamesListView;
     public VBox chatContainer;
+    private ChatBuilder chatBuilder;
+    private ChatController chatController;
+    private CreateGameFormBuilder createGameFormBuilder;
+    private Node gameForm;
 
     @Autowired
     public LobbyViewController(
@@ -121,7 +122,8 @@ public class LobbyViewController implements RootController, Terminable, Rincled
             @Nonnull final Property<Locale> selectedLocale,
             @Nullable final ApplicationState appState,
             @Nullable final Function<VBox, NotificationModalController> notificationRenderer
-    ) {
+    )
+    {
         this.lobbyChatClient = lobbyChatClient;
         this.logoutManager = logoutManager;
         this.alertBuilder = alertBuilder;
@@ -156,7 +158,8 @@ public class LobbyViewController implements RootController, Terminable, Rincled
         this.chatBuilder = chatBuilder;
     }
 
-    public void initialize() throws Exception {
+    public void initialize() throws Exception
+    {
         //Gives the cells of the ListViews a fixed height
         //Needed for cells which are empty to fit them to the height of filled cells
         lobbyGamesListView.setFixedCellSize(50);
@@ -176,31 +179,39 @@ public class LobbyViewController implements RootController, Terminable, Rincled
 
         configureSystemMessageManager();
 
-        if(Locale.getDefault().equals(Locale.GERMAN)) {
+        if (Locale.getDefault().equals(Locale.GERMAN))
+        {
             deButton.disableProperty().setValue(true);
-        }
-        else {
+        } else
+        {
             deButton.disableProperty().setValue(false);
         }
         enButton.disableProperty().bind(Bindings.when(deButton.disableProperty()).then(false).otherwise(true));
 
         JavaFXUtils.setButtonIcons(
-            createGameButton,
-            getClass().getResource("/assets/icons/navigation/addCircleWhite.png"),
-            getClass().getResource("/assets/icons/navigation/addCircleBlack.png"),
-            LobbyViewController.ICON_SIZE
+                createGameButton,
+                getClass().getResource("/assets/icons/navigation/addCircleWhite.png"),
+                getClass().getResource("/assets/icons/navigation/addCircleBlack.png"),
+                LobbyViewController.ICON_SIZE
         );
         JavaFXUtils.setButtonIcons(
-            logoutButton,
-            getClass().getResource("/assets/icons/navigation/exitWhite.png"),
-            getClass().getResource("/assets/icons/navigation/exitBlack.png"),
-            LobbyViewController.ICON_SIZE
+                logoutButton,
+                getClass().getResource("/assets/icons/navigation/exitWhite.png"),
+                getClass().getResource("/assets/icons/navigation/exitBlack.png"),
+                LobbyViewController.ICON_SIZE
         );
         JavaFXUtils.setButtonIcons(
-            armyBuilderLink,
-            getClass().getResource("/assets/icons/army/rallyTroopsWhite.png"),
-            getClass().getResource("/assets/icons/army/rallyTroopsBlack.png"),
-            LobbyViewController.ICON_SIZE
+                armyBuilderLink,
+                getClass().getResource("/assets/icons/army/rallyTroopsWhite.png"),
+                getClass().getResource("/assets/icons/army/rallyTroopsBlack.png"),
+                LobbyViewController.ICON_SIZE
+        );
+
+        JavaFXUtils.setButtonIcons(
+                bugReportButton,
+                getClass().getResource("/assets/icons/operation/bugReportWhite.png"),
+                getClass().getResource("/assets/icons/operation/bugReportBlack.png"),
+                40
         );
 
         musicManager.initButtonIcons(soundButton);
@@ -214,7 +225,8 @@ public class LobbyViewController implements RootController, Terminable, Rincled
         bindI18n();
         updateLabels(null);
 
-        if (appState != null) {
+        if (appState != null)
+        {
             JavaFXUtils.bindButtonDisableWithTooltip(
                     createGameButton,
                     createGameButtonContainer,
@@ -223,18 +235,21 @@ public class LobbyViewController implements RootController, Terminable, Rincled
             );
         }
 
-        if (Objects.nonNull(appState) && appState.notifications.size() > 0) {
+        if (Objects.nonNull(appState) && appState.notifications.size() > 0)
+        {
             showNotifications();
         }
     }
 
-    private void showNotifications() {
+    private void showNotifications()
+    {
         Objects.requireNonNull(appState);
         if (notificationRenderer == null) return;
 
         modalBackground.setVisible(true);
         final NotificationModalController modal = notificationRenderer.apply(this.modal);
-        modal.setOnDismiss((e, c) -> {
+        modal.setOnDismiss((e, c) ->
+        {
             modalBackground.setVisible(false);
             appState.notifications.clear();
             this.modal.getChildren().clear();
@@ -242,13 +257,15 @@ public class LobbyViewController implements RootController, Terminable, Rincled
         modal.setNotifications(appState.notifications);
     }
 
-    private void bindI18n() {
+    private void bindI18n()
+    {
         armyBuilderLink.textProperty().bind(
                 JavaFXUtils.bindTranslation(selectedLocale, "ArmyBuilderLink")
         );
     }
 
-    private void onLobbyOpen() {
+    private void onLobbyOpen()
+    {
         lobby.clearPlayers();
         lobby.clearGames();
 
@@ -259,17 +276,18 @@ public class LobbyViewController implements RootController, Terminable, Rincled
     private void setBackgroundImage()
     {
         Image backgroundImage = new Image(String.valueOf(getClass().getResource("/assets/splash.jpg")),
-                                          ProjectRbsgFXApplication.WIDTH, ProjectRbsgFXApplication.HEIGHT, true, true);
+                ProjectRbsgFXApplication.WIDTH, ProjectRbsgFXApplication.HEIGHT, true, true);
 
         mainStackPane.setBackground(new Background(new BackgroundImage(backgroundImage,
-                                                                   BackgroundRepeat.NO_REPEAT,
-                                                                   BackgroundRepeat.NO_REPEAT,
-                                                                   BackgroundPosition.CENTER,
-                                                                   BackgroundSize.DEFAULT)));
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.CENTER,
+                BackgroundSize.DEFAULT)));
 
     }
 
-    private void configureSystemMessageManager() throws Exception {
+    private void configureSystemMessageManager() throws Exception
+    {
         UserLeftMessageHandler userLeftMessageHandler = new UserLeftMessageHandler(this.lobby);
 
         UserJoinedMessageHandler userJoinedMessageHandler = new UserJoinedMessageHandler(this.lobby);
@@ -288,7 +306,8 @@ public class LobbyViewController implements RootController, Terminable, Rincled
         lobby.getSystemMessageManager().startSocket();
     }
 
-    private void withChatSupport() throws Exception {
+    private void withChatSupport() throws Exception
+    {
         if (chatBuilder != null)
         {
             final ViewComponent<ChatController> chatComponents = chatBuilder.buildChat(lobbyChatClient);
@@ -299,20 +318,26 @@ public class LobbyViewController implements RootController, Terminable, Rincled
 
     public void createGameButtonClicked()
     {
-        if(mainStackPane == null) {
+        if (mainStackPane == null)
+        {
             return;
         }
-        if (this.gameForm == null) {
-            try {
+        if (this.gameForm == null)
+        {
+            try
+            {
                 this.gameForm = this.createGameFormBuilder.getCreateGameForm();
 
-            } catch (IOException e) {
+            } catch (IOException e)
+            {
                 e.printStackTrace();
             }
         }
-        if ((this.gameForm != null) && (!mainStackPane.getChildren().contains(this.gameForm))) {
+        if ((this.gameForm != null) && (!mainStackPane.getChildren().contains(this.gameForm)))
+        {
             mainStackPane.getChildren().add(gameForm);
-        } else if ((this.gameForm != null) && (mainStackPane.getChildren().contains(this.gameForm))){
+        } else if ((this.gameForm != null) && (mainStackPane.getChildren().contains(this.gameForm)))
+        {
             this.gameForm.setVisible(true);
         }
 
@@ -327,11 +352,12 @@ public class LobbyViewController implements RootController, Terminable, Rincled
 
     private void updateLabels(Locale locale)
     {
-        if(Locale.getDefault().equals(locale))
+        if (Locale.getDefault().equals(locale))
         {
             return;
         }
-        if(locale != null) {
+        if (locale != null)
+        {
             selectedLocale.setValue(locale);
         }
 
@@ -340,7 +366,8 @@ public class LobbyViewController implements RootController, Terminable, Rincled
         deButton.textProperty().setValue(getResources().getString("deButton"));
         lobbyTitle.textProperty().setValue(getResources().getString("title"));
 
-        if(createGameFormBuilder != null && createGameFormBuilder.getCreateGameController() != null) {
+        if (createGameFormBuilder != null && createGameFormBuilder.getCreateGameController() != null)
+        {
             createGameFormBuilder.getCreateGameController().updateLabels();
         }
     }
@@ -381,5 +408,41 @@ public class LobbyViewController implements RootController, Terminable, Rincled
     public void goToArmyBuilder(ActionEvent actionEvent)
     {
         sceneManager.setScene(SceneManager.SceneIdentifier.ARMY_BUILDER, true, SceneManager.SceneIdentifier.LOBBY);
+    }
+
+    public void sendButReport(ActionEvent actionEvent)
+    {
+        String subject = "Bug%20Report";
+        String operation = "mailto:siebertgeorg@t-online.de?subject="+subject;
+
+        logger.debug("Email operation: " + operation);
+        Desktop desktop;
+
+        if(Desktop.isDesktopSupported() && (desktop = Desktop.getDesktop()).isSupported(Desktop.Action.MAIL)) {
+            try
+            {
+                URI mailto = new URI(operation);
+                desktop.mail(mailto);
+            } catch (URISyntaxException | IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        else {
+            logger.debug("Desktop is not supported on this system.");
+            String os = System.getProperty("os.name");
+            if(os.contains("Windows")) {
+                Runtime rt = Runtime.getRuntime();
+                try
+                {
+                    Process pr = rt.exec("cmd /c start " + operation);
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
     }
 }
