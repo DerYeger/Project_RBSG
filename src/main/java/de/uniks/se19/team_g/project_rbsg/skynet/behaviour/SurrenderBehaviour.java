0c50587515effb29ddd173c5e1fce1387e09c3be
millenium9977 <siebertgeorg@t-online.de>
2019-08-15 23:04:51 +0200

    Added SurrenderBehavior

---
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/skynet/behaviour/SurrenderBehaviour.java
@@ -0,0 +1,60 @@
+package de.uniks.se19.team_g.project_rbsg.skynet.behaviour;
+
+import de.uniks.se19.team_g.project_rbsg.ingame.model.Cell;
+import de.uniks.se19.team_g.project_rbsg.ingame.model.Game;
+import de.uniks.se19.team_g.project_rbsg.ingame.model.Player;
+import de.uniks.se19.team_g.project_rbsg.ingame.model.Unit;
+import de.uniks.se19.team_g.project_rbsg.skynet.action.Action;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class SurrenderBehaviour implements Behaviour
+{
+    @Override
+    public Optional<? extends Action> apply(Game game, Player player)
+    {
+        boolean cantAttackAnEnemyByType = isCantAttackAnEnemyByType(game, player);
+        boolean firstMovingPhase = game.getPhase().equals("movePhase");
+
+        if(cantAttackAnEnemyByType) {
+            return Optional.of(new SurrenderAction());
+        }
+
+        if(firstMovingPhase) {
+            boolean NoUnitCanMove = isNoUnitCanMove(player);
+            if(NoUnitCanMove) {
+                return Optional.of(new SurrenderAction());
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    private boolean isNoUnitCanMove(Player player)
+    {
+        return player
+                .getUnits()
+                .stream()
+                .allMatch( unit -> unit.getNeighbors().size() == getSizeOfPassableNeighbors(unit));
+    }
+
+    private int getSizeOfPassableNeighbors(Unit unit)
+    {
+        return (int) unit.getPosition().getNeighbors()
+                .stream()
+                .filter(Cell::isPassable).count();
+    }
+
+    private boolean isCantAttackAnEnemyByType(Game game, Player player)
+    {
+        return game
+                .getUnits()
+                .stream()
+                .filter(enemy -> enemy.getLeader() != player)
+                .noneMatch(enemy -> player
+                        .getUnits()
+                        .stream()
+                        .anyMatch(unit -> unit.canAttack(enemy)));
+    }
+}
