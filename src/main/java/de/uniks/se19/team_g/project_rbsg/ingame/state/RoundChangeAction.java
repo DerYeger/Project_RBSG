e64eabd10a03fe901535308d389e8ab59387d0d4
vakanto <vakanto@posteo.eu>
2019-08-27 13:34:47 +0200

    Enhanced listener

--- src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/state/RoundChangeAction.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/state/RoundChangeAction.java
@@ -3,17 +3,19 @@ package de.uniks.se19.team_g.project_rbsg.ingame.state;
 import de.uniks.se19.team_g.project_rbsg.ingame.model.Player;
 
 public class RoundChangeAction implements Action {
+
+    int roundCounter;
+
+    public RoundChangeAction(int roundCounter){
+        this.roundCounter=roundCounter;
+    }
     @Override
     public void undo() {
-
+        roundCounter--;
     }
 
     @Override
     public void run() {
-
-    }
-
-    public Player getPreviousPlayer(){
-
+        roundCounter++;
     }
 }
--- src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/state/RoundeChangeListener.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/state/RoundeChangeListener.java
@@ -1,27 +1,48 @@
 package de.uniks.se19.team_g.project_rbsg.ingame.state;
 
 import de.uniks.se19.team_g.project_rbsg.ingame.model.Game;
+import de.uniks.se19.team_g.project_rbsg.ingame.model.Player;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RoundeChangeListener implements GameEventDispatcher.Listener {
 
     private Game game;
+    private Player oldPlayer;
+    int round;
 
     RoundeChangeListener(){
-        game.getPhase();
+        //game.getPhase();
     }
 
     private void publishRoundChange(GameEventDispatcher dispatcher) {
         dispatcher.getModelManager().addAction(
-                new RoundChangeAction()
+                new RoundChangeAction(round)
         );
     }
 
     @Override
     public void accept(GameEvent gameEvent, GameEventDispatcher gameEventDispatcher) {
         if(!(gameEvent instanceof  GameChangeObjectEvent)){
-            return
+            return;
         }
+
+        Object roundChangeObject = gameEventDispatcher.getModelManager().getEntityById(((GameChangeObjectEvent) gameEvent).getEntityId());
+
+        if(!(roundChangeObject instanceof Game)){
+            return;
+        }
+        System.out.println(roundChangeObject.toString());
+        Game game = (Game) roundChangeObject;
+
+        if(game.getPhase()==null){
+            return;
+        }
+        if(game.getPhase().equals("movePhase") && game.getCurrentPlayer()!=oldPlayer){
+            oldPlayer=game.getCurrentPlayer();
+            RoundChangeAction roundChangeAction = new RoundChangeAction(round);
+            publishRoundChange(gameEventDispatcher);
+        }
+
     }
 }
