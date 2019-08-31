38c40215186e7201d58a058390fcd4e506283803
Jan Müller <janmueller3698@gmail.com>
2019-08-31 18:04:02 +0200

    Added moving Unit to MovementOption

--- src/main/java/de/uniks/se19/team_g/project_rbsg/skynet/behaviour/movement/MovementOption.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/skynet/behaviour/movement/MovementOption.java
@@ -2,17 +2,23 @@ package de.uniks.se19.team_g.project_rbsg.skynet.behaviour.movement;
 
 import de.uniks.se19.team_g.project_rbsg.ingame.battlefield.Tour;
 import de.uniks.se19.team_g.project_rbsg.ingame.model.Cell;
+import de.uniks.se19.team_g.project_rbsg.ingame.model.Unit;
 import org.springframework.lang.NonNull;
 
 public class MovementOption {
 
+    public final Unit unit;
+
     public final Tour tour;
     public final Cell destination;
+
     public final Enemy enemy;
     public final Double distanceToEnemy;
 
-    public MovementOption(@NonNull final Tour tour,
+    public MovementOption(@NonNull final Unit unit,
+                          @NonNull final Tour tour,
                           @NonNull final Enemy enemy) {
+        this.unit = unit;
         this.tour = tour;
         this.destination = tour.getTarget();
         this.enemy = enemy;
