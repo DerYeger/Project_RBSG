e416b30e172f8b2efaf71d29a59446d0eeb9eb22
Jan Müller <janmueller3698@gmail.com>
2019-08-31 18:08:42 +0200

    WIP implementation

--- src/main/java/de/uniks/se19/team_g/project_rbsg/skynet/behaviour/movement/CompositeMovementOptionEvaluator.kt
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/skynet/behaviour/movement/CompositeMovementOptionEvaluator.kt
@@ -1,27 +1,51 @@
 package de.uniks.se19.team_g.project_rbsg.skynet.behaviour.movement
 
-import de.uniks.se19.team_g.project_rbsg.ingame.battlefield.Tour
-import org.springframework.lang.NonNull
+import de.uniks.se19.team_g.project_rbsg.ingame.model.Cell
+import de.uniks.se19.team_g.project_rbsg.ingame.model.Unit
 
 class CompositeMovementOptionEvaluator : MovementOptionEvaluator {
 
-    override fun compare(@NonNull first: MovementOption,
-                         @NonNull second: MovementOption): Int {
+    override fun compare(first: MovementOption,
+                         second: MovementOption): Int {
+        return when {
+            first.tour === second.tour -> compareEnemies(first, second)
+            first.enemy === second.enemy -> compareTours(first, second)
+            else -> compareMovementOptions(first, second)
+        }
+    }
+
+    private fun compareEnemies(first: MovementOption,
+                               second: MovementOption) : Int {
         return 0
     }
 
-    private fun compareMovementOptions(@NonNull first: MovementOption,
-                                       @NonNull second: MovementOption) {
+    private fun compareTours(first: MovementOption,
+                             second: MovementOption) : Int {
+        if (first.distanceToEnemy != second.distanceToEnemy)
+            return (first.distanceToEnemy - second.distanceToEnemy).toInt()
 
+        return compareDestinations(first, second)
     }
 
-    private fun compareTours(@NonNull first: Tour,
-                             @NonNull second: Tour) {
 
+    private fun compareMovementOptions(first: MovementOption,
+                                       second: MovementOption) : Int {
+        return 0
     }
 
-    private fun compareEnemies(@NonNull first: Enemy,
-                               @NonNull second: Enemy) {
+    private fun compareDestinations(first: MovementOption,
+                                    second: MovementOption) : Int {
+        val firstThreats = first.destination.threateningNeighbors(first.unit).size
+        val secondThreats = second.destination.threateningNeighbors(second.unit).size
+        if (firstThreats != secondThreats)
+            return firstThreats - secondThreats
 
+        return 0
+    }
+
+    private fun Cell.threateningNeighbors(unit : Unit): List<Unit> {
+        return this.neighbors
+                .mapNotNull { it.unit }
+                .filter { it.canAttack(unit) }
     }
 }
