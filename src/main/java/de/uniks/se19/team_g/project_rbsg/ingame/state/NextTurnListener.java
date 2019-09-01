492b56a005d909b11339ac28953246c295aa0818
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-09-01 21:00:28 +0200

    NextTurnListener now expects two events to be fired before adding a next turn event on the history stack

--- src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/state/NextTurnListener.java
+++ src/main/java/de/uniks/se19/team_g/project_rbsg/ingame/state/NextTurnListener.java
@@ -1,109 +1,101 @@
 package de.uniks.se19.team_g.project_rbsg.ingame.state;
 
 import de.uniks.se19.team_g.project_rbsg.ingame.model.Game;
+import de.uniks.se19.team_g.project_rbsg.ingame.model.ModelManager;
 import de.uniks.se19.team_g.project_rbsg.ingame.model.Player;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
+import javax.annotation.Nonnull;
+import java.util.concurrent.CompletableFuture;
 
 @Component
 public class NextTurnListener implements GameEventDispatcher.Listener {
 
-    private Player lastPlayer;
-    private boolean newPlayerFlag;
-    private Game game;
-    private boolean isFirstPlayer;
-    private ArrayList<Player> donePlayers;
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
-    NextTurnListener(){
-        lastPlayer=null;
-        isFirstPlayer=false;
-        donePlayers = new ArrayList<>();
+    // it's fine. we initialize them with expectNextTurn
+    @SuppressWarnings("NullableProblems")
+    @Nonnull
+    private CompletableFuture<ModelManager> phaseExpectation;
+    @SuppressWarnings("NullableProblems")
+    @Nonnull
+    private CompletableFuture<Player> playerExpectation;
+    @SuppressWarnings("NullableProblems")
+    @Nonnull
+    private CompletableFuture<Void> combined;
+
+    public NextTurnListener(){
+        expectNextTurn();
     }
 
-    private void publishNextTurnChange(GameEventDispatcher dispatcher, Player player) {
-        dispatcher.getModelManager().addAction(
-                new NextTurnAction(player.getCurrentGame().getTurnCounter(), player)
+    private void expectNextTurn() {
+        phaseExpectation = new CompletableFuture<>();
+        playerExpectation = new CompletableFuture<>();
+
+        combined = phaseExpectation.thenCombine(
+            playerExpectation,
+            this::publishNextTurn
         );
+        combined
+            .exceptionally(throwable -> {logger.error("failed waiting for next player event", throwable); return null;})
+            .thenRun(this::expectNextTurn);
+    }
+
+    private Void publishNextTurn(ModelManager modelManager, Player player) {
+        modelManager.addAction(
+                new NextTurnAction(modelManager.getGame(), player)
+        );
+
+        return null;
     }
 
     @Override
-    public void accept(GameEvent gameEvent, GameEventDispatcher gameEventDispatcher) {
+    public void accept(GameEvent gameEvent, GameEventDispatcher dispatcher) {
 
         if(!(gameEvent instanceof  GameChangeObjectEvent)){
             return;
         }
 
-        Object mayBeAGame = gameEventDispatcher.getModelManager().getEntityById(((GameChangeObjectEvent) gameEvent).getEntityId());
-        Object maybeAPlayer = gameEventDispatcher.getModelManager().getEntityById(((GameChangeObjectEvent) gameEvent).getNewValue());
-        String gamePhase;
-        Player player;
+        GameChangeObjectEvent changeEvent = (GameChangeObjectEvent) gameEvent;
+        Object entity = dispatcher.getModelManager().getEntityById(changeEvent.getEntityId());
 
-        if(!(mayBeAGame instanceof Game)){
-            if(this.game!=null){
-                gamePhase=this.game.getPhase();
-                player= game.getCurrentPlayer();
+        if (!(entity instanceof Game)) {
+            return;
+        }
+        Game game = (Game) entity;
+
+        if(isFirstMovementPhaseEvent(changeEvent)) {
+            if (phaseExpectation.isDone()) {
+                combined.completeExceptionally(new IllegalStateException("received multiple times in a row firstMovementPhase event without a player change"));
             }
-            else {
+            phaseExpectation.complete(dispatcher.getModelManager());
+        }
+
+        if (isNextPlayerEvent(changeEvent)) {
+
+            Object player = dispatcher.getModelManager().getEntityById(changeEvent.getNewValue());
+            if (!(player instanceof Player)) {
+                // doesn't make sense
+                logger.error("received unknown player id as next player");
                 return;
             }
-        }
-        else{
-            this.game = (Game) mayBeAGame;
-            gamePhase=this.game.getPhase();
-            player = this.game.getCurrentPlayer();
-        }
-
-        if(game.getTurnCounter().get() == 0 && donePlayers.size()==0 && player!=null){
-            donePlayers.add(player);
-        }
-
-        if(gamePhase==null){
-            return;
-        }
-        if(game.getPhase().equals("movePhase") && !donePlayers.contains(player)){
-            System.out.println(game.getPhase());
-            publishNextTurnChange(gameEventDispatcher, player);
-        }
-        if(game.getPlayers().size()==donePlayers.size()){
-            donePlayers.clear();
-        }
-
-        /*
-        if(!(maybeAPlayer instanceof Player) && lastPlayer==null){
-            return;
-        }
-        else{
-            if((maybeAPlayer instanceof Player)){
-                Player candidate = (Player) maybeAPlayer;
-                newPlayerFlag = isNewPlayer(candidate);
-
-                if(lastPlayer==null && !isFirstPlayer){
-                    lastPlayer = candidate;
-                    newPlayerFlag=false;
-                    isFirstPlayer = true;
-                }
-                if(newPlayerFlag){
-                    lastPlayer = candidate;
-                    isFirstPlayer=false;
-                }
+            if (playerExpectation.isDone()) {
+                combined.completeExceptionally(new IllegalStateException("received multiple times in a row firstMovementPhase event without a player change"));
             }
+            playerExpectation.complete((Player) player);
         }
-        if(gamePhase==null){
-            return;
-        }
-
-        if(gamePhase.equals("movePhase") && newPlayerFlag){
-            publishNextTurnChange(gameEventDispatcher, lastPlayer);
-            newPlayerFlag=false;
-        }*/
-
     }
 
-    private boolean isNewPlayer(Player player){
-        if(!player.equals(lastPlayer)){
-            return true;
-        }
-        return false;
+    private boolean isNextPlayerEvent(GameChangeObjectEvent changeEvent) {
+        return "currentPlayer".equals(changeEvent.getFieldName());
+    }
+
+    private boolean isFirstMovementPhaseEvent(GameChangeObjectEvent changeEvent) {
+        return
+            "phase".equals(changeEvent.getFieldName()) &&
+            Game.Phase.movePhase.name().equals(changeEvent.getNewValue())
+        ;
     }
 }
